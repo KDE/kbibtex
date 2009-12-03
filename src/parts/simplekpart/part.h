0c8cb0bec2c8263b8757c8a2cc587d0f66e0e963
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2009-12-03 21:50:55 +0000

    Minor codestyle improvements
    svn path=/trunk/; revision=106

--- src/parts/simplekpart/part.h
+++ src/parts/simplekpart/part.h
@@ -35,7 +35,7 @@ public:
     KBibTeXPart(QWidget *parentWidget, QObject *parent, bool browserViewWanted);
     virtual ~KBibTeXPart();
 
-protected: // KParts::ReadOnlyPart API
+protected:
     virtual bool openFile();
     virtual bool saveFile();
 
--- src/program/mdiwidget.h
+++ src/program/mdiwidget.h
@@ -29,7 +29,7 @@
 
 namespace KParts
 {
-    class Part;
+class Part;
 }
 
 namespace KBibTeX
