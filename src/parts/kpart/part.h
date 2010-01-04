750045a6bee65a267cf591af3e2d64982a9f84d1
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2010-01-04 22:31:11 +0000

    Moving from "simple" kpart to a normal kpart
    svn path=/trunk/; revision=136

---
+++ src/parts/kpart/part.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+*   Copyright (C) 2004-2009 by Thomas Fischer                             *
+*   fischer@unix-ag.uni-kl.de                                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#ifndef KBIBTEX_PART_PART_H
+#define KBIBTEX_PART_PART_H
+
+#include <kparts/part.h>
+
+#include <bibtexeditor.h>
+
+class KBibTeXPart : public KParts::ReadWritePart
+{
+    Q_OBJECT
+
+    friend class KBibTeXBrowserExtension;
+
+public:
+    KBibTeXPart(QWidget *parentWidget, QObject *parent, bool browserViewWanted);
+    virtual ~KBibTeXPart();
+
+protected:
+    virtual bool openFile();
+    virtual bool saveFile();
+
+protected:
+    void setupActions(bool BrowserViewWanted);
+    void fitActionSettings();
+
+    /*
+      protected Q_SLOTS: // action slots
+        void onSelectAll();
+        void onUnselect();
+        void onSetCoding( int Coding );
+        void onSetEncoding( int Encoding );
+        void onSetShowsNonprinting( bool on );
+        void onSetResizeStyle( int Style );
+        void onToggleOffsetColumn( bool on );
+        void onToggleValueCharColumns( int VisibleColunms );
+    */
+
+    /*
+      private Q_SLOTS:
+        // used to catch changes in the bytearray widget
+        void onSelectionChanged( bool HasSelection );
+    */
+
+protected slots:
+    void saveDocumentDialog();
+
+private:
+    class KBibTeXPartPrivate;
+    KBibTeXPartPrivate * const d;
+};
+
+#endif // KBIBTEX_PART_PART_H
---
+++ src/parts/kpart/partfactory.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+*   Copyright (C) 2004-2009 by Thomas Fischer                             *
+*   fischer@unix-ag.uni-kl.de                                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#ifndef KBIBTEX_PART_PARTFACTORY_H
+#define KBIBTEX_PART_PARTFACTORY_H
+
+#include <kparts/factory.h>
+
+class KComponentData;
+
+class KBibTeXPartFactory : public KParts::Factory
+{
+    Q_OBJECT
+
+public:
+    KBibTeXPartFactory();
+    virtual ~KBibTeXPartFactory();
+
+public:
+    virtual KParts::Part* createPartObject(QWidget *parentWidget, QObject *parent, const char *classname, const QStringList &args);
+
+public:
+    static const KComponentData &componentData();
+};
+
+#endif // KBIBTEX_PART_PARTFACTORY_H
