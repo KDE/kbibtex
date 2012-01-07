b5567357950b272ed4939a63d295c23dab4a23b9
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-01-07 20:58:36 +0000

    Rewrote extraction of PubMed IDs from result, potentially fixing bug 19234
    svn path=/trunk/; revision=1028

--- src/networking/onlinesearch/onlinesearchpubmed.cpp
+++ src/networking/onlinesearch/onlinesearchpubmed.cpp
@@ -168,11 +168,18 @@ void OnlineSearchPubMed::eSearchDone()
 
         if (!result.contains(QLatin1String("<Count>0</Count>"))) {
             /// without parsing XML text correctly, just extract all PubMed ids
-            static const QRegExp regExpId("<Id>(\\d+)</Id>", Qt::CaseInsensitive);
-            int p = -1;
             QStringList idList;
-            while ((p = result.indexOf(regExpId, p + 1)) >= 0)
-                idList << regExpId.cap(1);
+            int p1, p2;
+            /// All IDs are within <IdList>...</IdList>
+            if ((p1 = result.indexOf(QLatin1String("<IdList>"))) > 0 && (p2 = result.indexOf(QLatin1String("</IdList>"), p1)) > 0) {
+                int p3, p4 = p1;
+                /// Search for each <Id>...</Id>
+                while ((p3 = result.indexOf(QLatin1String("<Id>"), p4)) > 0 && (p4 = result.indexOf(QLatin1String("</Id>"), p3)) > 0 && p4 < p2) {
+                    /// Extract ID and add it to list
+                    const QString id = result.mid(p3 + 4, p4 - p3 - 4);
+                    idList << id;
+                }
+            }
 
             if (idList.isEmpty()) {
                 kDebug() << "No ids here:" << squeeze_text(result.simplified(), 100);
