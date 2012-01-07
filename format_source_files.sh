fb7876a5dbc8e8b2621aae4bb02a3c09085ffe91
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-01-07 20:34:44 +0000

    Apply cmake pretty printer if available
    svn path=/trunk/; revision=1027

--- format_source_files.sh
+++ format_source_files.sh
@@ -4,3 +4,6 @@ export LC_ALL=en_US.utf8
 export LANG=C
 
 astyle --indent=spaces=4 --brackets=linux --indent-labels --pad=oper --unpad=paren --one-line=keep-statements --convert-tabs --indent-preprocessor $(find src -type f -name '*.cpp' -o -name '*.h')
+
+export CMAKEPP=$(which cmakepp)
+test -x "${CMAKEPP}" && find -name CMakeLists.txt -exec ${CMAKEPP} --sob 0 --overwrite '{}' ';'
