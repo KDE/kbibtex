7e54b7c962abeff5b353d278bb9499f7b6821694
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-03-18 18:09:53 +0000

    Working on issues with build system and determining SVN revision
    svn path=/trunk/; revision=1203

---
+++ src/getsvn.cmake
@@ -0,0 +1,105 @@
+# Inspired by:
+# http://stackoverflow.com/questions/3780667/use-cmake-to-get-build-time-svn-revision
+
+
+if ( DEFINED ENV{SVN_REV} )
+    message(
+        STATUS
+        "SVN version fetched from environment"
+    )
+    set(
+        SVN_REVISION
+        $ENV{SVN_REV}
+    )
+endif ( DEFINED ENV{SVN_REV} )
+
+if( NOT DEFINED SVN_REVISION)
+    find_program(
+        SVNVERSION_EXECUTABLE
+        svnversion
+    )
+    if(
+        SVNVERSION_EXECUTABLE
+    )
+        message(
+            STATUS
+            "Extracting SVN version, please wait..."
+        )
+        execute_process(
+            COMMAND
+            ${SVNVERSION_EXECUTABLE}
+            -n
+            ${SOURCE_DIR}
+            OUTPUT_VARIABLE
+            SVN_REVISION
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        if(
+            SVN_REVISION
+            MATCHES
+            "exported"
+        )
+            message(
+                STATUS
+                "Not an SVN version, assuming non-developer release"
+            )
+            set(
+                SVN_REVISION
+                "unknown"
+            )
+        endif(
+            SVN_REVISION
+            MATCHES
+            "exported"
+        )
+    else(
+        SVNVERSION_EXECUTABLE
+    )
+        message(
+            STATUS
+            "No Subversion installed, assuming non-developer release"
+        )
+        set(
+            SVN_REVISION
+            "unknown"
+        )
+    endif(
+        SVNVERSION_EXECUTABLE
+    )
+endif( NOT DEFINED SVN_REVISION )
+
+# write a file with the SVNVERSION define
+file(WRITE "${BINARY_DIR}/src/version.h.tmp" "#ifndef VERSION_H\n")
+file(APPEND "${BINARY_DIR}/src/version.h.tmp" "#define VERSION_H\n")
+file(APPEND "${BINARY_DIR}/src/version.h.tmp" "const char *versionNumber = \"SVN revision ${SVN_REVISION}\";\n")
+file(APPEND "${BINARY_DIR}/src/version.h.tmp" "#endif // VERSION_H\n")
+
+message(
+    STATUS
+    "SVN version is "
+    ${SVN_REVISION}
+)
+
+IF(EXISTS "${BINARY_DIR}/src/version.h.tmp")
+execute_process(
+    COMMAND
+    ${CMAKE_COMMAND}
+    -E
+    copy_if_different
+    "${BINARY_DIR}/src/version.h.tmp"
+    "${BINARY_DIR}/src/version.h"
+    COMMAND
+    ${CMAKE_COMMAND}
+    -E
+    remove
+    "${BINARY_DIR}/src/version.h.tmp"
+    WORKING_DIRECTORY
+    "${CMAKE_SOURCE_DIR}"
+)
+else(EXISTS "${BINARY_DIR}/src/version.h.tmp")
+message(
+    STATUS
+    "${BINARY_DIR}/src/version.h.tmp does not exist"
+)
+endIF(EXISTS "${BINARY_DIR}/src/version.h.tmp")
