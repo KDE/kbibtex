34fd4ad1c5b2022a40bab01c176cfd0f55c299a2
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-04-22 17:25:33 +0000

    Updating formatting script to support more recent versions of astyle
    svn path=/trunk/; revision=1222

--- format_source_files.sh
+++ format_source_files.sh
@@ -3,7 +3,7 @@
 export LC_ALL=en_US.utf8
 export LANG=C
 
-astyle --indent=spaces=4 --brackets=linux --indent-labels --pad=oper --unpad=paren --one-line=keep-statements --convert-tabs --indent-preprocessor $(find src -type f -name '*.cpp' -o -name '*.h')
+astyle --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor $(find src -type f -name '*.cpp' -o -name '*.h')
 
 export CMAKEPP=$(which cmakepp)
 test -x "${CMAKEPP}" && find -name CMakeLists.txt -exec ${CMAKEPP} --sob 0 --overwrite '{}' ';'
