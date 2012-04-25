7a955ad0ffeb6eba9998ef920c37ccae27389634
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-04-25 18:11:19 +0000

    Updating formatting script
    svn path=/trunk/; revision=1237

--- format_source_files.sh
+++ format_source_files.sh
@@ -3,7 +3,11 @@
 export LC_ALL=en_US.utf8
 export LANG=C
 
-astyle --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor $(find src -type f -name '*.cpp' -o -name '*.h')
+astyle --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor $(find src -type f -name '*.cpp' -o -name '*.h')
+
+# correct some astyle mis-formattings
+find src -name '*.h' -exec sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' '{}' ';'
+find src -name '*.cpp' -exec sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' '{}' ';'
 
 export CMAKEPP=$(which cmakepp)
 test -x "${CMAKEPP}" && find -name CMakeLists.txt -exec ${CMAKEPP} --sob 0 --overwrite '{}' ';'
