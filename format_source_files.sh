b2c0d6a6abd42b49e7bec18a9e0b89d65e23bd4c
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-05-05 09:33:13 +0000

    Improving formatting script to be more efficient
    svn path=/trunk/; revision=1244

--- format_source_files.sh
+++ format_source_files.sh
@@ -3,11 +3,21 @@
 export LC_ALL=en_US.utf8
 export LANG=C
 
-astyle --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor $(find src -type f -name '*.cpp' -o -name '*.h')
+find src -type f -name '*.cpp' -o -name '*.h' | xargs svn status | while read status filename ; do
+	if [[ ${status} != "M" ]] ; then continue ; fi
+	if [[ ! -s "${filename}" ]] ; then continue ; fi
 
-# correct some astyle mis-formattings
-find src -name '*.h' -exec sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' '{}' ';'
-find src -name '*.cpp' -exec sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' '{}' ';'
+	echo "Processing \"${filename}\""
+	astyle --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor "${filename}"
 
+	# correct some astyle mis-formattings
+	if [[ "${filename}" != "${filename/%.h/}" ]] ; then
+		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' "${filename}"
+	elif [[ "${filename}" != "${filename/%.cpp/}" ]] ; then
+		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' "${filename}"
+	fi
+done
+
+echo "Processing \"CMakeLists.txt\" files"
 export CMAKEPP=$(which cmakepp)
 test -x "${CMAKEPP}" && find -name CMakeLists.txt -exec ${CMAKEPP} --sob 0 --overwrite '{}' ';'
