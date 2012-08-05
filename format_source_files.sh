e2c53dd240391d21f258e97decc65c11472d2822
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-08-05 13:58:02 +0000

    Backward porting formatting script for .cpp/.h source files
    svn path=/branches/0.4/; revision=1313

--- format_source_files.sh
+++ format_source_files.sh
@@ -3,4 +3,18 @@
 export LC_ALL=en_US.utf8
 export LANG=C
 
-astyle --indent=spaces=4 --brackets=linux --indent-labels --pad=oper --unpad=paren --one-line=keep-statements --convert-tabs --indent-preprocessor $(find src -type f -name '*.cpp' -o -name '*.h')
+find src -type f -name '*.cpp' -o -name '*.h' | xargs svn status | while read status filename ; do
+	if [[ ${status} != "M" ]] ; then continue ; fi
+	if [[ ! -s "${filename}" ]] ; then continue ; fi
+
+	echo "Processing \"${filename}\""
+	astyle --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor "${filename}"
+
+	# correct some astyle mis-formattings
+	if [[ "${filename}" != "${filename/%.h/}" ]] ; then
+		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' "${filename}"
+	elif [[ "${filename}" != "${filename/%.cpp/}" ]] ; then
+		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' "${filename}"
+	fi
+done
+
