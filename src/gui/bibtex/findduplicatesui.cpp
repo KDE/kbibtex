a707bfa02b84420359a543631f9b06f93bfb9a60
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2012-10-06 09:15:46 +0000

    Updating GUI strings
    svn path=/trunk/; revision=1409

--- src/gui/bibtex/findduplicatesui.cpp
+++ src/gui/bibtex/findduplicatesui.cpp
@@ -554,9 +554,9 @@ public:
         containerLayout->addStretch(10);
         labelWhichClique = new QLabel(p);
         containerLayout->addWidget(labelWhichClique);
-        buttonPrev = new KPushButton(KIcon("go-previous"), i18n("Previous"), p);
+        buttonPrev = new KPushButton(KIcon("go-previous"), i18n("Previous Clique"), p);
         containerLayout->addWidget(buttonPrev, 1);
-        buttonNext = new KPushButton(KIcon("go-next"), i18n("Next"), p);
+        buttonNext = new KPushButton(KIcon("go-next"), i18n("Next Clique"), p);
         containerLayout->addWidget(buttonNext, 1);
 
         filterModel = new FilterIdBibTeXFileModel(p);
