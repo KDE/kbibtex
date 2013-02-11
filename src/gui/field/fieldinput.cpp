86af22aa01c36fc7bf7d9fc9e3901bdacd9b7753
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-02-11 19:09:22 +0000

    Fixing minor compilation issue
    svn path=/trunk/; revision=1567

--- src/gui/field/fieldinput.cpp
+++ src/gui/field/fieldinput.cpp
@@ -215,7 +215,7 @@ public:
 
     bool selectCrossRef() {
         Q_ASSERT_X(fieldLineEdit != NULL, "void FieldInput::FieldInputPrivate::selectCrossRef()", "fieldLineEdit is invalid");
-        if (bibtexFile == NULL) return;
+        if (bibtexFile == NULL) return false;
 
         /// create a standard input dialog with a list of all keys (ids of entries)
         bool ok = false;
