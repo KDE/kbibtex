f9ecec46375b6801484a5a19d34a727cce74f03a
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-03-09 18:45:53 +0000

    Updating copyright to 2013
    svn path=/trunk/; revision=1593

--- src/program/docklets/statistics.h
+++ src/program/docklets/statistics.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2012 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2013 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
