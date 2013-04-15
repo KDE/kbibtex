e823815f2fe4a8cf6842ca39601d933381fc8fee
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-04-15 19:49:00 +0000

    Bug 20610: Show svn revision number and branch even for Git forks; patch 17645, slightly revised
    svn path=/trunk/; revision=1609

--- src/getsvn.cmake
+++ src/getsvn.cmake
@@ -1,129 +1,63 @@
 # Inspired by:
 # http://stackoverflow.com/questions/3780667/use-cmake-to-get-build-time-svn-revision
 
-if(
-    DEFINED
-    ENV{SVN_REV}
-)
-    message(
-        STATUS
-        "SVN version was set outside through environment variable SVN_REV"
-    )
-    set(
-        SVN_REVISION
-        $ENV{SVN_REV}
-    )
-endif(
-    DEFINED
-    ENV{SVN_REV}
-)
+if (DEFINED ENV{SVN_REV})
+    message (STATUS "SVN version set by environment variable SVN_REV")
+    set (SVN_REVISION $ENV{SVN_REV})
+    if (DEFINED ENV{SVN_PATH})
+        message (STATUS "SVN path set by environment variable SVN_PATH")
+	set (SVN_OUTPUT $ENV{SVN_PATH}) # set SVN_OUTPUT here, will be processed to SVN_PATH later
+    endif (DEFINED ENV{SVN_PATH})
+else (DEFINED ENV{SVN_REV})
+    set(SVN_REVISION "unknown")
+    if (EXISTS ${SOURCE_DIR}/.svn)
+        set(SCM "svn")
+    elseif (EXISTS ${SOURCE_DIR}/.git)
+        set(SCM "git")
+    else (EXISTS ${SOURCE_DIR}/.svn)
+        message(STATUS "No SCM directory found. Assuming non-developer release.")
+    endif (EXISTS ${SOURCE_DIR}/.svn)
+    
+    
+    # SVN
+    if (SCM MATCHES "svn" )
+        find_program (SVN_EXECUTABLE NAMES svn svn.exe svn.bat)
+        find_program (SVNVERSION_EXECUTABLE NAMES svnversion svnversion.exe svnversion.bat)
+        if (SVN_EXECUTABLE AND SVNVERSION_EXECUTABLE )
+            execute_process (COMMAND ${SVNVERSION_EXECUTABLE} -n ${SOURCE_DIR}
+                             OUTPUT_VARIABLE SVN_REVISION
+                             OUTPUT_STRIP_TRAILING_WHITESPACE)
+            execute_process (COMMAND ${SVN_EXECUTABLE} info ${SOURCE_DIR}
+                             OUTPUT_VARIABLE SVN_OUTPUT
+                             OUTPUT_STRIP_TRAILING_WHITESPACE)
+        else (SVN_EXECUTABLE AND SVNVERSION_EXECUTABLE )
+            message( "SVN configuration found, but no SVN executable." )
+        endif (SVN_EXECUTABLE AND SVNVERSION_EXECUTABLE )
+    endif (SCM MATCHES "svn" )
+    
+    
+    # Git
+    if (SCM MATCHES "git" )
+        find_program( GIT_EXECUTABLE NAMES git )  # FIXME git.exe? -> not tested on windows
+        if (GIT_EXECUTABLE)
+            execute_process (COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+                             OUTPUT_VARIABLE GIT_REV_TREEISH
+                             OUTPUT_STRIP_TRAILING_WHITESPACE)
+            execute_process (COMMAND ${GIT_EXECUTABLE} svn find-rev ${GIT_REV_TREEISH}
+                             OUTPUT_VARIABLE SVN_REVISION
+                             OUTPUT_STRIP_TRAILING_WHITESPACE)
+            execute_process (COMMAND ${GIT_EXECUTABLE} svn info
+                             OUTPUT_VARIABLE SVN_OUTPUT
+                             OUTPUT_STRIP_TRAILING_WHITESPACE)
+        else (GIT_EXECUTABLE)
+            message( "Git configuration found, but no git executable." )
+        endif (GIT_EXECUTABLE)
+    endif( SCM MATCHES "git" )
+endif (DEFINED ENV{SVN_REV})
 
-if(
-   EXISTS ${SOURCE_DIR}/.svn
-)
-    if(
-        NOT
-        DEFINED
-        SVN_REVISION
-    )
-        find_program(
-            SVNVERSION_EXECUTABLE
-            NAMES svnversion svnversion.exe svnversion.bat
-        )
-        if(
-            SVNVERSION_EXECUTABLE
-        )
-            message(
-                STATUS
-                "Extracting SVN version ..."
-            )
-            execute_process(
-                COMMAND
-                ${SVNVERSION_EXECUTABLE}
-                -n
-                ${SOURCE_DIR}
-                OUTPUT_VARIABLE
-                SVN_REVISION
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-            )
-
-            if(
-                SVN_REVISION
-                MATCHES
-                "exported"
-            )
-                message(
-                    STATUS
-                    "Not an SVN version, assuming non-developer release"
-                )
-                set(
-                    SVN_REVISION
-                    "unknown"
-                )
-            else(
-                SVN_REVISION
-                MATCHES
-                "exported"
-            )
-                find_program(
-                    SVN_EXECUTABLE
-                    NAMES svn svn.exe svn.bat
-                )
-                if(
-                    SVN_EXECUTABLE
-                )
-                    message(
-                        STATUS
-                        "Extracting detailed SVN information ..."
-                    )
-                    execute_process(
-                        COMMAND
-                        ${SVN_EXECUTABLE} info
-                        ${SOURCE_DIR}
-                        OUTPUT_VARIABLE
-                        SVN_OUTPUT
-                        OUTPUT_STRIP_TRAILING_WHITESPACE
-                    )
-                    string(
-                        REGEX MATCH "(trunk|branches|tags)(/.*)?" SVN_PATH ${SVN_OUTPUT}
-                    )
-                endif(
-                    SVN_EXECUTABLE
-                )
-        endif(
-                SVN_REVISION
-                MATCHES
-                "exported"
-            )
-        else(
-            SVNVERSION_EXECUTABLE
-        )
-            message(
-                STATUS
-                "No Subversion installed, assuming non-developer release"
-            )
-            set(
-                SVN_REVISION
-                "unknown"
-            )
-        endif(
-            SVNVERSION_EXECUTABLE
-        )
-    endif(
-        NOT
-        DEFINED
-        SVN_REVISION
-    )
-else(
-   EXISTS ${SOURCE_DIR}/.svn
-)
-    set(
-        SVN_REVISION
-        "unknown"
-    )
-endif(
-   EXISTS ${SOURCE_DIR}/.svn
-)
+if (DEFINED SVN_OUTPUT)
+    string( REGEX MATCH "(trunk|branches|tags)(/.*)?" SVN_PATH ${SVN_OUTPUT})
+endif (DEFINED SVN_OUTPUT)
 
 # write a file with the SVNVERSION define
 file(
