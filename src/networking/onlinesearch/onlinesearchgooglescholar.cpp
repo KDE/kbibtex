5653e44af78e3c0b1e7b10db1a7e614629aa2d26
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-06-18 20:59:20 +0000

    Switching from deprecated fromAscii/toAscii to fromLatin1/toLatin1
    svn path=/trunk/; revision=1665

--- src/networking/onlinesearch/onlinesearchgooglescholar.cpp
+++ src/networking/onlinesearch/onlinesearchgooglescholar.cpp
@@ -224,11 +224,11 @@ void OnlineSearchGoogleScholar::doneFetchingSetConfigPage()
 
     if (handleErrors(reply)) {
         KUrl url(QString(d->queryPageUrl).arg(reply->url().host()));
-        url.addEncodedQueryItem(QString("as_q").toAscii(), d->queryFreetext.toAscii());
-        url.addEncodedQueryItem(QString("as_sauthors").toAscii(), d->queryAuthor.toAscii());
-        url.addEncodedQueryItem(QString("as_ylo").toAscii(), d->queryYear.toAscii());
-        url.addEncodedQueryItem(QString("as_yhi").toAscii(), d->queryYear.toAscii());
-        url.addEncodedQueryItem(QString("as_vis").toAscii(), "1"); ///< include citations
+        url.addEncodedQueryItem(QString("as_q").toLatin1(), d->queryFreetext.toLatin1());
+        url.addEncodedQueryItem(QString("as_sauthors").toLatin1(), d->queryAuthor.toLatin1());
+        url.addEncodedQueryItem(QString("as_ylo").toLatin1(), d->queryYear.toLatin1());
+        url.addEncodedQueryItem(QString("as_yhi").toLatin1(), d->queryYear.toLatin1());
+        url.addEncodedQueryItem(QString("as_vis").toLatin1(), "1"); ///< include citations
         url.addQueryItem("num", QString::number(d->numResults));
         url.addQueryItem("btnG", "Search Scholar");
 
