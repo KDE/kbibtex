cfff68ceae1906bd181460f76ededee0783f475e
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-09-27 19:59:30 +0000

    Backport from trunk (r1765, r1768): Reverting FilterBar's GUI back to previous state with multiple combobox lists instead of nested submenus; various fixes
    svn path=/branches/0.5/; revision=1778

--- src/gui/widgets/filterbar.h
+++ src/gui/widgets/filterbar.h
@@ -1,5 +1,5 @@
 /***************************************************************************
-*   Copyright (C) 2004-2012 by Thomas Fischer                             *
+*   Copyright (C) 2004-2013 by Thomas Fischer                             *
 *   fischer@unix-ag.uni-kl.de                                             *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
@@ -39,7 +39,12 @@ public:
     SortFilterBibTeXFileModel::FilterQuery filter();
 
 public slots:
-    void setFilter(SortFilterBibTeXFileModel::FilterQuery);
+    /**
+     * Set the filter critera to be both shown in this filter bar
+     * and applied to the list of elements.
+     * @param fq query data structure to be used
+     */
+    void setFilter(SortFilterBibTeXFileModel::FilterQuery fq);
 
 signals:
     void filterChanged(SortFilterBibTeXFileModel::FilterQuery);
@@ -49,7 +54,7 @@ private:
     FilterBarPrivate *d;
 
 private slots:
-    void stateChanged();
+    void comboboxStatusChanged();
     void resetState();
     void userPressedEnter();
     void publishFilter();
