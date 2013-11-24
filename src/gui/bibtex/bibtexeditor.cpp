5ef0f27a46ff71527b915b70280b35a7d71a3c23
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-11-24 19:15:46 +0100

    Backport from 70c8793c49bbf5522005770511438c81cf15458c Fixing wrongful initialization of a QSharedPointer using NULL leading to "invalid write" as detected by Valgrind on 64-bit architectures

--- src/gui/bibtex/bibtexeditor.cpp
+++ src/gui/bibtex/bibtexeditor.cpp
@@ -104,7 +104,7 @@ private:
 const QString ElementEditorDialog::configGroupNameWindowSize = QLatin1String("ElementEditorDialog");
 
 BibTeXEditor::BibTeXEditor(const QString &name, QWidget *parent)
-        : BibTeXFileView(name, parent), m_isReadOnly(false), m_current(NULL), m_filterBar(NULL), m_elementEditorDialog(NULL), m_elementEditor(NULL)
+    : BibTeXFileView(name, parent), m_isReadOnly(false), m_current(QSharedPointer<Element>()), m_filterBar(NULL), m_elementEditorDialog(NULL), m_elementEditor(NULL)
 {
     connect(this, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(itemActivated(QModelIndex)));
 }
