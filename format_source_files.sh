6d63b875281ba27cf950ea3af3b4524e504a31e0
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-12-05 22:47:26 +0100

    Normalizing SIGNAL statements (removing spaces, "const" and "*") to using some grep and sed magic

--- format_source_files.sh
+++ format_source_files.sh
@@ -11,6 +11,15 @@ find src -type f -name '*.cpp' -o -name '*.h' | xargs git status | while read ha
 	echo "Processing \"${filename}\""
 	astyle --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor "${filename}"
 
+	# normalize SIGNAL statements as recommended at
+	# http://marcmutz.wordpress.com/effective-qt/prefer-to-use-normalised-signalslot-signatures/
+	# astyle would insert spaces etc
+	grep -Po 'SIGNAL\([^(]+\([^)]*\)\)' "${1}" | while read original ; do
+		normalized="$(sed -e 's/const //g;s/&//g;s/ //g;s/\//\\\//g' <<<${original})"
+		originalregexpized="$(sed -e 's/\*/\\*/g;s/\//\\\//g' <<<${original})"
+		sed -i -e 's/'"${originalregexpized}"'/'"${normalized}"'/g' "${1}"
+	done
+
 	# correct some astyle mis-formattings
 	if [[ "${filename}" != "${filename/%.h/}" ]] ; then
 		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' "${filename}"
