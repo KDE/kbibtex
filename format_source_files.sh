0b98ddc2b5f06294c3d1c95620de838cea780b00
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-12-11 22:12:15 +0100

    Only touching original .cpp or .h file during formatting if it has been actually changed by the formatting process, thus avoiding "modified on disk" messages in certain editors.

--- format_source_files.sh
+++ format_source_files.sh
@@ -8,8 +8,10 @@ find src -type f -name '*.cpp' -o -name '*.h' | xargs git status | while read ha
 	if [[ ${status} != "modified:" && ${status} != "added:" ]] ; then continue ; fi
 	if [[ ! -s "${filename}" ]] ; then continue ; fi
 
+	TEMPFILE=$(mktemp 'kbibtex_formatted_source_XXXXXXXXXXX.cpp')
+
 	echo "Processing \"${filename}\""
-	astyle -n --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor "${filename}"
+	astyle -n --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor <"${filename}" >${TEMPFILE}
 
 	# normalize SIGNAL statements as recommended at
 	# http://marcmutz.wordpress.com/effective-qt/prefer-to-use-normalised-signalslot-signatures/
@@ -17,14 +19,19 @@ find src -type f -name '*.cpp' -o -name '*.h' | xargs git status | while read ha
 	grep -Po '(SIGNAL|SLOT)\([^(]+\([^)]*\)\)' "${filename}" | while read original ; do
 		normalized="$(sed -e 's/const //g;s/&//g;s/ //g;s/\//\\\//g' <<<${original})"
 		originalregexpized="$(sed -e 's/\*/\\*/g;s/\//\\\//g' <<<${original})"
-		sed -i -e 's/'"${originalregexpized}"'/'"${normalized}"'/g' "${filename}"
+		sed -i -e 's/'"${originalregexpized}"'/'"${normalized}"'/g' ${TEMPFILE}
 	done
 
 	# correct some astyle mis-formattings
 	if [[ "${filename}" != "${filename/%.h/}" ]] ; then
-		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' "${filename}"
+		sed -i -e 's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' ${TEMPFILE}
 	elif [[ "${filename}" != "${filename/%.cpp/}" ]] ; then
-		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' "${filename}"
+		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' ${TEMPFILE}
 	fi
+
+	# only change/touch original file if it has been changed
+	diff -q ${TEMPFILE} "${filename}" >/dev/null || cp -p ${TEMPFILE} "${filename}"
+
+	rm -f ${TEMPFILE}
 done
 
