339505b29c36518c47f0b9f20a90c556f1e0454e
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-12-12 21:04:45 +0100

    Backporting parts of the following commits:
    ef3a48eb4c4b7f19b3c617baeb5449b882e9a7dc
    Finding modified files using Git instead of Subversion
    
    8bd0ff93205ef42271250cf7c38f450f805763b4
    Fixing typo in formatting script
    
    58ed034185da1a6573968e721efa52436518dc73
    Omit making backup files when formatting code, we have Git
    
    0b98ddc2b5f06294c3d1c95620de838cea780b00
    Only touching original .cpp or .h file during formatting if it has been
    actually changed by the formatting process, thus avoiding
    "modified on disk" messages in certain editors.

--- format_source_files.sh
+++ format_source_files.sh
@@ -3,18 +3,26 @@
 export LC_ALL=en_US.utf8
 export LANG=C
 
-find src -type f -name '*.cpp' -o -name '*.h' | xargs svn status | while read status filename ; do
-	if [[ ${status} != "M" && ${status} != "A" ]] ; then continue ; fi
+find src -type f -name '*.cpp' -o -name '*.h' | xargs git status | while read hash status filename ; do
+	if [[ ${hash} != "#" ]] ; then continue ; fi
+	if [[ ${status} != "modified:" && ${status} != "added:" ]] ; then continue ; fi
 	if [[ ! -s "${filename}" ]] ; then continue ; fi
 
+	TEMPFILE=$(mktemp 'kbibtex_formatted_source_XXXXXXXXXXX.cpp')
+
 	echo "Processing \"${filename}\""
-	astyle --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor "${filename}"
+	astyle -n --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor <"${filename}" >${TEMPFILE}
 
 	# correct some astyle mis-formattings
 	if [[ "${filename}" != "${filename/%.h/}" ]] ; then
-		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' "${filename}"
+		sed -i -e 's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' ${TEMPFILE}
 	elif [[ "${filename}" != "${filename/%.cpp/}" ]] ; then
-		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' "${filename}"
+		sed -i -e 's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' ${TEMPFILE}
 	fi
+
+	# only change/touch original file if it has been changed
+	diff -q ${TEMPFILE} "${filename}" >/dev/null || cp -p ${TEMPFILE} "${filename}"
+
+	rm -f ${TEMPFILE}
 done
 
