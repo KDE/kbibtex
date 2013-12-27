9dcd40f6815cf48ced09a75e3fc9e7efe93bdc57
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2013-12-27 20:58:14 +0100

    Adding nice icons

--- src/networking/zotero/tagmodel.cpp
+++ src/networking/zotero/tagmodel.cpp
@@ -69,6 +69,9 @@ QVariant TagModel::data(const QModelIndex &index, int role) const
             return dataKeys[index.row()];
         else if (index.column() == 1)
             return data.value(dataKeys[index.row()]);
+    } else if (role == Qt::DecorationRole) {
+        if (index.column() == 0)
+            return KIcon("mail-tagged");
     } else if (role == TagRole)
         return dataKeys[index.row()];
     else if (role == TagCountRole)
