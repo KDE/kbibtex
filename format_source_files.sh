673c9321dd1c82afdd7c12031ebc3e5f22bae190
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2014-01-07 21:41:39 +0100

    Making formatting script more sophisticated: - Use "getopt" to parse command line options - Provide a "--help" option - Provide a "--all" option to format all source files, not just those   seen by git as added or modified

--- format_source_files.sh
+++ format_source_files.sh
@@ -4,16 +4,78 @@ export LC_ALL=en_US.utf8
 export LANG=C
 MY_NAME="$(basename "$0")"
 
-find src -type f -name '*.cpp' -o -name '*.h' | xargs git status --untracked-files=no --porcelain | while read status filename ; do
-	if [[ ${status} != "M" && ${status} != "A" && ${status} != "AM" && ${status} != "MM" ]] ; then continue ; fi
+SET_VERBOSE=0
+ALL_FILES=0
+
+function parsearguments {
+	local ARGS=$(getopt -o "h" -l "verbose,all,help" -n "${MY_NAME}" -- "$@")
+	local PRINT_HELP=0
+
+	#Bad arguments
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+
+	# A little magic
+	eval set -- "$ARGS"
+
+	while true ; do
+		case "$1" in
+		-h|--help)
+			PRINT_HELP=1
+			shift
+		;;
+		--verbose )
+			SET_VERBOSE=1
+			shift
+		;;
+		--all )
+			ALL_FILES=1
+			shift
+		;;
+		--)
+			shift
+			break
+		esac
+	done
+
+	if [[ ${PRINT_HELP} -ne 0 ]] ; then
+		echo "Usage: ${MY_NAME} [--all] [--verbose]" >&2
+		echo "Format C++ source code and headers in a git-controlled directory structure." >&2
+		echo >&2
+		echo "Options:" >&2
+		echo " --help" >&2
+		echo "    Show this help" >&2
+		echo " --verbose" >&2
+		echo "    Show verbose (more) output" >&2
+		echo " --all" >&2
+		echo "    Format all .cpp/.h files, not just those marked by git as added or modified">&2
+		exit 1
+	fi
+}
+
+parsearguments "$@"
+
+if [[ ${ALL_FILES} -eq 0 ]] ; then
+	# Format only files seen by git as added or modified,
+	# filter for .cpp or .h files.
+	# Trying to handle filenames with spaces correctly (not tested)
+	git status --untracked-files=no --porcelain | awk '/^\s*[AM]+\s+.*[.](cpp|h)$/ {for(i=2;i<NR;++i){printf $i" "};printf $NR"\n"}'
+else
+	# Format all .cpp or .h files; using "find" to located those files
+	find . -type f -name '*.cpp' -o -name '*.h'
+fi | \
+# Sort files alphabetically, omit duplicates
+sort -u | \
+while read filename ; do
 	if [[ ! -s "${filename}" ]] ; then
-		echo "${MY_NAME}: File not found: \"${filename}\" (state: ${status})" >&2
+		echo "${MY_NAME}: File not found: \"${filename}\"" >&2
 		continue
 	fi
 
 	TEMPFILE=$(mktemp 'kbibtex_formatted_source_XXXXXXXXXXX.cpp')
 
-	echo "Processing \"${filename}\" (state: ${status})"
+	echo "Processing \"${filename}\""
 	astyle -n --align-reference=name --align-pointer=name --indent=spaces=4 --brackets=linux --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs --indent-preprocessor <"${filename}" >${TEMPFILE}
 
 	# normalize SIGNAL statements as recommended at
