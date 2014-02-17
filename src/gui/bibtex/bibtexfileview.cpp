62a3c39cfb92e35e626755a9df7e4e93d490bb3a
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2014-02-17 08:46:41 +0100

    Applying patch 19942 from GNA bug report 21581: https://gna.org/bugs/?21581 https://gna.org/bugs/download.php?file_id=19942 This patch was contributed by Jurgen Spitzmuller.
    Original comment:
    
    As noted in the context of another report, the column sorting of the BibTeXFile view is not restored over sessions.
    
    Here is a fix. There were two issues:
    
    1. header session state was restored too early and then apparently overwritten by other header initialization processes. Moving the session restoration to the end of the constructor fixes this.
    
    2. toggling the sorting did not trigger storeColumns, so a change of state was not recorded (unless other actions such as column width change were performed as well).

--- src/gui/bibtex/bibtexfileview.cpp
+++ src/gui/bibtex/bibtexfileview.cpp
@@ -163,15 +163,6 @@ BibTeXFileView::BibTeXFileView(const QString &name, QWidget *parent)
     setAllColumnsShowFocus(true);
     setRootIsDecorated(false);
 
-    /// restore header appearance
-    KConfigGroup configGroup(d->config, d->configGroupName);
-    QByteArray headerState = configGroup.readEntry(d->configHeaderState.arg(d->name), QByteArray());
-    if (headerState.isEmpty())
-        d->resetColumnsToDefault();
-    else {
-        header()->restoreState(headerState);
-        d->storeColumns();
-    }
     header()->setStretchLastSection(false);
 
     /// header appearance and behaviour
@@ -220,6 +211,16 @@ BibTeXFileView::BibTeXFileView(const QString &name, QWidget *parent)
     action = new KAction(i18n("No sorting"), header());
     connect(action, SIGNAL(triggered()), this, SLOT(noSorting()));
     header()->addAction(action);
+
+    /// restore header appearance
+    KConfigGroup configGroup(d->config, d->configGroupName);
+    QByteArray headerState = configGroup.readEntry(d->configHeaderState.arg(d->name), QByteArray());
+    if (headerState.isEmpty())
+        d->resetColumnsToDefault();
+    else {
+        header()->restoreState(headerState);
+        d->storeColumns();
+    }
 }
 
 BibTeXFileView::~BibTeXFileView()
@@ -302,8 +303,10 @@ void BibTeXFileView::headerResetToDefaults()
 void BibTeXFileView::sort(int t, Qt::SortOrder s)
 {
     SortFilterBibTeXFileModel *sortedModel = dynamic_cast<SortFilterBibTeXFileModel *>(model());
-    if (sortedModel != NULL)
+    if (sortedModel != NULL) {
         sortedModel->sort(t, s);
+        d->storeColumns();
+    }
 }
 
 void BibTeXFileView::noSorting()
@@ -312,5 +315,6 @@ void BibTeXFileView::noSorting()
     if (sortedModel != NULL) {
         sortedModel->sort(-1);
         header()->setSortIndicator(-1, Qt::AscendingOrder);
+        d->storeColumns();
     }
 }
