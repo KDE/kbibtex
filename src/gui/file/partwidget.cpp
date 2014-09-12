KDE/kbibtex

commit 0d79f0c5f8b64ef8dd5b6b0b22e4e2e80d86edde

Refactoring filter bar into KPart
Refactoring the filter from its previous place inside a toolbar into the
KPart's widget right above the main list view.
One advantage is that each open file can have its own filter settings.

src/parts/part.cpp
@@ -485,12 +485,22 @@ KBibTeXPart::KBibTeXPart(QWidget *parentWidget, QObject *parent, bool browserVie
     setComponentData(KBibTeXPartFactory::componentData());
     setObjectName("KBibTeXPart::KBibTeXPart");
 
+    QWidget *container = new QWidget(parentWidget);
+    QBoxLayout *containerLayout = new QVBoxLayout(container);
+    containerLayout->setMargin(0);
+
+    d->filterBar = new FilterBar(container);
+    containerLayout->addWidget(d->filterBar, 0);
+
     // TODO Setup view
-    d->editor = new FileView(QLatin1String("Main"), parentWidget);
+    d->editor = new FileView(QLatin1String("Main"), container);
+    containerLayout->addWidget(d->editor, 0xffffff);
+    d->editor->setFilterBar(d->filterBar);
     d->editor->setReadOnly(!isReadWrite());
     d->editor->setItemDelegate(new FileDelegate(d->editor));
-    setWidget(d->editor);
     connect(d->editor, SIGNAL(modified()), this, SLOT(setModified()));
+
+    setWidget(container);
 
     setupActions(browserViewWanted);
 
@@ -535,12 +545,9 @@ void KBibTeXPart::setupActions(bool /*browserViewWanted FIXME*/)
     actionCollection()->addAction("file_save_copy_as", saveCopyAsAction);
     connect(saveCopyAsAction, SIGNAL(triggered()), this, SLOT(documentSaveCopyAs()));
 
-    d->filterBar = new FilterBar(0);
-    d->editor->setFilterBar(d->filterBar);
     KAction *filterWidgetAction = new KAction(i18n("Filter"), this);
     actionCollection()->addAction("toolbar_filter_widget", filterWidgetAction);
     filterWidgetAction->setShortcut(Qt::CTRL + Qt::Key_F);
-    filterWidgetAction->setDefaultWidget(d->filterBar);
     connect(filterWidgetAction, SIGNAL(triggered()), d->filterBar, SLOT(setFocus()));
 
     KActionMenu *newElementAction = new KActionMenu(KIcon("address-book-new"), i18n("New element"), this);

src/gui/widgets/filterbar.cpp
@@ -175,16 +175,13 @@ FilterBar::FilterBar(QWidget *parent)
         : QWidget(parent), d(new FilterBarPrivate(this))
 {
     QBoxLayout *layout = new QHBoxLayout(this);
-    layout->setMargin(1);
-
-    layout->addSpacing(4);
+    layout->setMargin(0);
 
     QLabel *label = new QLabel(i18n("Filter:"), this);
     layout->addWidget(label, 0);
 
     d->comboBoxFilterText = new KComboBox(true, this);
     label->setBuddy(d->comboBoxFilterText);
-    setFocusProxy(d->comboBoxFilterText);
     layout->addWidget(d->comboBoxFilterText, 5);
     d->comboBoxFilterText->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
     d->comboBoxFilterText->setEditable(true);
@@ -220,8 +217,6 @@ FilterBar::FilterBar(QWidget *parent)
     d->buttonClearAll->setIcon(KIcon("edit-clear-locationbar-rtl"));
     d->buttonClearAll->setToolTip(i18n("Reset filter criteria"));
     layout->addWidget(d->buttonClearAll, 0);
-
-    layout->addSpacing(4);
 
     connect(d->comboBoxFilterText->lineEdit(), SIGNAL(textChanged(QString)), d->delayedTimer, SLOT(trigger()));
     connect(d->comboBoxFilterText->lineEdit(), SIGNAL(returnPressed()), this, SLOT(userPressedEnter()));

src/parts/kbibtexpartui.rc
@@ -31,7 +31,4 @@
   <Separator/>
   <Action name="element_new" />
 </ToolBar>
-<ToolBar name="filterToolBar"><text>Filter Toolbar</text>
-  <Action name="toolbar_filter_widget" />
-</ToolBar>
 </kpartgui>

src/program/docklets/referencepreview.cpp
@@ -395,8 +395,8 @@ void ReferencePreview::linkClicked(const QUrl &url)
             fq.terms << text.mid(p + 1);
             fq.combination = SortFilterFileModel::EveryTerm;
             fq.field = text.left(p);
-            d->fileView->setFilterBarFilter(fq);
             fq.searchPDFfiles = false;
+            d->fileView->setFilterBarFilter(fq);
         }
     }
 }

src/program/kbibtexui.rc
@@ -24,6 +24,4 @@
     <Separator/>
     <Merge/>
 </ToolBar>
-<ToolBar name="filterToolBar"><text>Filter Toolbar</text>
-</ToolBar>
 </kpartgui>

commit 197046f6f4047d4d113e2fd1102460d03aefd4ec

Fixing regression introduced by refactoring filter bar
Commit 0d79f0c5f8b64e broke with the assumption that the KPart's widget
is always of class FileView. As it is now a plain QWidget acting as a
container for multiple objects, various dynamic and object casts return
NULL.
This commit tries to address this issue, by using a new class for the
KPart's widget, which allows to extract the FileView object in a similar
manner as was possible earlier.

src/gui/CMakeLists.txt
@@ -28,6 +28,7 @@ set(
     field/fieldlistedit.cpp
     field/colorlabelwidget.cpp
     file/fileview.cpp
+    file/partwidget.cpp
     file/findduplicatesui.cpp
     file/clipboard.cpp
     file/filemodel.cpp
@@ -85,6 +86,7 @@ set(
     file/clipboard.h
     file/filemodel.h
     file/fileview.h
+    file/partwidget.h
     element/findpdfui.h
     element/elementeditor.h
     element/associatedfilesui.h

src/gui/file/partwidget.cpp
@@ -1,0 +1,61 @@
+/***************************************************************************
+ *   Copyright (C) 2004-2014 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#include "partwidget.h"
+
+#include <QLayout>
+
+#include "filterbar.h"
+#include "fileview.h"
+
+class PartWidget::Private
+{
+private:
+    PartWidget *p;
+
+public:
+    FileView *fileView;
+    FilterBar *filterBar;
+
+    Private(PartWidget *parent)
+            : p(parent) {
+        QBoxLayout *layout = new QVBoxLayout(parent);
+        layout->setMargin(0);
+
+        filterBar = new FilterBar(parent);
+        layout->addWidget(filterBar, 0);
+
+        fileView = new FileView(QLatin1String("Main"), parent);
+        layout->addWidget(fileView, 0xffffff);
+        fileView->setFilterBar(filterBar);
+        fileView->setItemDelegate(new FileDelegate(fileView));
+        connect(fileView, SIGNAL(modified()), parent, SLOT(setModified()));
+    }
+};
+
+PartWidget::PartWidget(QWidget *parent)
+        : QWidget(parent), d(new PartWidget::Private(this)) {
+    /// nothing
+}
+
+FileView *PartWidget::fileView() {
+    return d->fileView;
+}
+
+FilterBar *PartWidget::filterBar() {
+    return d->filterBar;
+}

src/gui/file/partwidget.h
@@ -1,0 +1,45 @@
+/***************************************************************************
+ *   Copyright (C) 2004-2014 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+
+#ifndef KBIBTEX_GUI_PARTWIDGET_H
+#define KBIBTEX_GUI_PARTWIDGET_H
+
+#include <QWidget>
+
+#include "kbibtexgui_export.h"
+
+class FileView;
+class FilterBar;
+
+/**
+ * @author Thomas Fischer <fischer@unix-ag.uni-kl.de>
+ */
+class KBIBTEXGUI_EXPORT PartWidget : public QWidget {
+    Q_OBJECT
+
+public:
+    PartWidget(QWidget *parent);
+
+    FileView *fileView();
+    FilterBar *filterBar();
+
+private:
+    class Private;
+    Private *const d;
+};
+
+#endif // KBIBTEX_GUI_PARTWIDGET_H

src/parts/part.cpp
@@ -78,6 +78,7 @@
 #include "clipboard.h"
 #include "idsuggestions.h"
 #include "partfactory.h"
+#include "fileview.h"
 // #include "browserextension.h" // FIXME
 
 static const char RCFileName[] = "kbibtexpartui.rc";
@@ -94,10 +95,9 @@ private:
 
 public:
     File *bibTeXFile;
-    FileView *editor;
+    PartWidget *partWidget;
     FileModel *model;
     SortFilterFileModel *sortFilterProxyModel;
-    FilterBar *filterBar;
     QSignalMapper *signalMapperNewElement;
     KAction *editCutAction, *editDeleteAction, *editCopyAction, *editPasteAction, *editCopyReferencesAction, *elementEditAction, *elementViewDocumentAction, *fileSaveAction, *elementFindPDFAction, *entryApplyDefaultFormatString;
     KMenu *viewDocumentMenu;
@@ -190,8 +190,8 @@ public:
         if (sortFilterProxyModel != NULL) delete sortFilterProxyModel;
         sortFilterProxyModel = new SortFilterFileModel(p);
         sortFilterProxyModel->setSourceModel(model);
-        editor->setModel(sortFilterProxyModel);
-        connect(filterBar, SIGNAL(filterChanged(SortFilterFileModel::FilterQuery)), sortFilterProxyModel, SLOT(updateFilter(SortFilterFileModel::FilterQuery)));
+        partWidget->fileView()->setModel(sortFilterProxyModel);
+        connect(partWidget->filterBar(), SIGNAL(filterChanged(SortFilterFileModel::FilterQuery)), sortFilterProxyModel, SLOT(updateFilter(SortFilterFileModel::FilterQuery)));
     }
 
     bool openFile(const KUrl &url, const QString &localFilePath) {
@@ -224,12 +224,12 @@ public:
         bibTeXFile->setProperty(File::Url, QUrl(url));
 
         model->setBibliographyFile(bibTeXFile);
-        editor->setModel(model);
+        partWidget->fileView()->setModel(model);
         if (sortFilterProxyModel != NULL) delete sortFilterProxyModel;
         sortFilterProxyModel = new SortFilterFileModel(p);
         sortFilterProxyModel->setSourceModel(model);
-        editor->setModel(sortFilterProxyModel);
-        connect(filterBar, SIGNAL(filterChanged(SortFilterFileModel::FilterQuery)), sortFilterProxyModel, SLOT(updateFilter(SortFilterFileModel::FilterQuery)));
+        partWidget->fileView()->setModel(sortFilterProxyModel);
+        connect(partWidget->filterBar(), SIGNAL(filterChanged(SortFilterFileModel::FilterQuery)), sortFilterProxyModel, SLOT(updateFilter(SortFilterFileModel::FilterQuery)));
 
         if (url.isLocalFile())
             fileSystemWatcher.addPath(url.pathOrUrl());
@@ -323,7 +323,7 @@ public:
             return false;
 
         /// export bibliography data into temporary file
-        SortFilterFileModel *model = qobject_cast<SortFilterFileModel *>(editor->model());
+        SortFilterFileModel *model = qobject_cast<SortFilterFileModel *>(partWidget->fileView()->model());
         Q_ASSERT_X(model != NULL, "bool KBibTeXPart::KBibTeXPartPrivate:saveFile(const KUrl &url)", "SortFilterFileModel *model from editor->model() is invalid");
         FileExporter *exporter = fileExporterFactory(url);
 
@@ -415,9 +415,9 @@ public:
         viewDocumentMenu->clear();
         int result = 0;
 
-        QSharedPointer<const Entry> entry = editor->currentElement().dynamicCast<const Entry>();
+        QSharedPointer<const Entry> entry = partWidget->fileView()->currentElement().dynamicCast<const Entry>();
         if (!entry.isNull()) {
-            QList<KUrl> urlList = FileInfo::entryUrls(entry.data(), editor->fileModel()->bibliographyFile()->property(File::Url).toUrl(), FileInfo::TestExistanceYes);
+            QList<KUrl> urlList = FileInfo::entryUrls(entry.data(), partWidget->fileView()->fileModel()->bibliographyFile()->property(File::Url).toUrl(), FileInfo::TestExistanceYes);
             if (!urlList.isEmpty()) {
                 /// First iteration: local references only
                 KAction *firstAction = NULL;
@@ -466,14 +466,14 @@ public:
         KConfigGroup configGroup(config, Preferences::groupUserInterface);
         const Preferences::ElementDoubleClickAction doubleClickAction = (Preferences::ElementDoubleClickAction)configGroup.readEntry(Preferences::keyElementDoubleClickAction, (int)Preferences::defaultElementDoubleClickAction);
 
-        disconnect(editor, SIGNAL(elementExecuted(QSharedPointer<Element>)), editor, SLOT(editElement(QSharedPointer<Element>)));
-        disconnect(editor, SIGNAL(elementExecuted(QSharedPointer<Element>)), p, SLOT(elementViewDocument()));
+        disconnect(partWidget->fileView(), SIGNAL(elementExecuted(QSharedPointer<Element>)), partWidget->fileView(), SLOT(editElement(QSharedPointer<Element>)));
+        disconnect(partWidget->fileView(), SIGNAL(elementExecuted(QSharedPointer<Element>)), p, SLOT(elementViewDocument()));
         switch (doubleClickAction) {
         case Preferences::ActionOpenEditor:
-            connect(editor, SIGNAL(elementExecuted(QSharedPointer<Element>)), editor, SLOT(editElement(QSharedPointer<Element>)));
+            connect(partWidget->fileView(), SIGNAL(elementExecuted(QSharedPointer<Element>)), partWidget->fileView(), SLOT(editElement(QSharedPointer<Element>)));
             break;
         case Preferences::ActionViewDocument:
-            connect(editor, SIGNAL(elementExecuted(QSharedPointer<Element>)), p, SLOT(elementViewDocument()));
+            connect(partWidget->fileView(), SIGNAL(elementExecuted(QSharedPointer<Element>)), p, SLOT(elementViewDocument()));
             break;
         }
     }
@@ -485,22 +485,9 @@ KBibTeXPart::KBibTeXPart(QWidget *parentWidget, QObject *parent, bool browserVie
     setComponentData(KBibTeXPartFactory::componentData());
     setObjectName("KBibTeXPart::KBibTeXPart");
 
-    QWidget *container = new QWidget(parentWidget);
-    QBoxLayout *containerLayout = new QVBoxLayout(container);
-    containerLayout->setMargin(0);
-
-    d->filterBar = new FilterBar(container);
-    containerLayout->addWidget(d->filterBar, 0);
-
-    // TODO Setup view
-    d->editor = new FileView(QLatin1String("Main"), container);
-    containerLayout->addWidget(d->editor, 0xffffff);
-    d->editor->setFilterBar(d->filterBar);
-    d->editor->setReadOnly(!isReadWrite());
-    d->editor->setItemDelegate(new FileDelegate(d->editor));
-    connect(d->editor, SIGNAL(modified()), this, SLOT(setModified()));
-
-    setWidget(container);
+    d->partWidget = new PartWidget(parentWidget);
+    d->partWidget->fileView()->setReadOnly(!isReadWrite());
+    setWidget(d->partWidget);
 
     setupActions(browserViewWanted);
 
@@ -548,7 +535,7 @@ void KBibTeXPart::setupActions(bool /*browserViewWanted FIXME*/)
     KAction *filterWidgetAction = new KAction(i18n("Filter"), this);
     actionCollection()->addAction("toolbar_filter_widget", filterWidgetAction);
     filterWidgetAction->setShortcut(Qt::CTRL + Qt::Key_F);
-    connect(filterWidgetAction, SIGNAL(triggered()), d->filterBar, SLOT(setFocus()));
+    connect(filterWidgetAction, SIGNAL(triggered()), d->partWidget->filterBar(), SLOT(setFocus()));
 
     KActionMenu *newElementAction = new KActionMenu(KIcon("address-book-new"), i18n("New element"), this);
     actionCollection()->addAction("element_new", newElementAction);
@@ -572,7 +559,7 @@ void KBibTeXPart::setupActions(bool /*browserViewWanted FIXME*/)
     d->elementEditAction = new KAction(KIcon("document-edit"), i18n("Edit Element"), this);
     d->elementEditAction->setShortcut(Qt::CTRL + Qt::Key_E);
     actionCollection()->addAction(QLatin1String("element_edit"),  d->elementEditAction);
-    connect(d->elementEditAction, SIGNAL(triggered()), d->editor, SLOT(editCurrentElement()));
+    connect(d->elementEditAction, SIGNAL(triggered()), d->partWidget->fileView(), SLOT(editCurrentElement()));
     d->elementViewDocumentAction = new KAction(KIcon("application-pdf"), i18n("View Document"), this);
     d->elementViewDocumentAction->setShortcut(Qt::CTRL + Qt::Key_D);
     actionCollection()->addAction(QLatin1String("element_viewdocument"),  d->elementViewDocumentAction);
@@ -586,7 +573,7 @@ void KBibTeXPart::setupActions(bool /*browserViewWanted FIXME*/)
     actionCollection()->addAction(QLatin1String("entry_applydefaultformatstring"), d->entryApplyDefaultFormatString);
     connect(d->entryApplyDefaultFormatString, SIGNAL(triggered()), this, SLOT(applyDefaultFormatString()));
 
-    Clipboard *clipboard = new Clipboard(d->editor);
+    Clipboard *clipboard = new Clipboard(d->partWidget->fileView());
 
     d->editCopyReferencesAction = new KAction(KIcon("edit-copy"), i18n("Copy References"), this);
     d->editCopyReferencesAction->setShortcut(Qt::CTRL + Qt::SHIFT + Qt::Key_C);
@@ -596,7 +583,7 @@ void KBibTeXPart::setupActions(bool /*browserViewWanted FIXME*/)
     d->editDeleteAction = new KAction(KIcon("edit-table-delete-row"), i18n("Delete"), this);
     d->editDeleteAction->setShortcut(Qt::Key_Delete);
     actionCollection()->addAction(QLatin1String("edit_delete"),  d->editDeleteAction);
-    connect(d->editDeleteAction, SIGNAL(triggered()), d->editor, SLOT(selectionDelete()));
+    connect(d->editDeleteAction, SIGNAL(triggered()), d->partWidget->fileView(), SLOT(selectionDelete()));
 
     d->editCutAction = actionCollection()->addAction(KStandardAction::Cut, clipboard, SLOT(cut()));
     d->editCopyAction = actionCollection()->addAction(KStandardAction::Copy, clipboard, SLOT(copy()));
@@ -604,36 +591,36 @@ void KBibTeXPart::setupActions(bool /*browserViewWanted FIXME*/)
     d->editPasteAction = actionCollection()->addAction(KStandardAction::Paste, clipboard, SLOT(paste()));
 
     /// build context menu for central BibTeX file view
-    d->editor->setContextMenuPolicy(Qt::ActionsContextMenu);
-    d->editor->addAction(d->elementEditAction);
-    d->editor->addAction(d->elementViewDocumentAction);
+    d->partWidget->fileView()->setContextMenuPolicy(Qt::ActionsContextMenu);
+    d->partWidget->fileView()->addAction(d->elementEditAction);
+    d->partWidget->fileView()->addAction(d->elementViewDocumentAction);
     QAction *separator = new QAction(this);
     separator->setSeparator(true);
-    d->editor->addAction(separator);
-    d->editor->addAction(d->editCutAction);
-    d->editor->addAction(d->editCopyAction);
-    d->editor->addAction(d->editCopyReferencesAction);
-    d->editor->addAction(d->editPasteAction);
-    d->editor->addAction(d->editDeleteAction);
+    d->partWidget->fileView()->addAction(separator);
+    d->partWidget->fileView()->addAction(d->editCutAction);
+    d->partWidget->fileView()->addAction(d->editCopyAction);
+    d->partWidget->fileView()->addAction(d->editCopyReferencesAction);
+    d->partWidget->fileView()->addAction(d->editPasteAction);
+    d->partWidget->fileView()->addAction(d->editDeleteAction);
     separator = new QAction(this);
     separator->setSeparator(true);
-    d->editor->addAction(separator);
+    d->partWidget->fileView()->addAction(separator);
 
     // TODO
 
-    connect(d->editor, SIGNAL(selectedElementsChanged()), this, SLOT(updateActions()));
-    connect(d->editor, SIGNAL(currentElementChanged(QSharedPointer<Element>,File*)), this, SLOT(updateActions()));
+    connect(d->partWidget->fileView(), SIGNAL(selectedElementsChanged()), this, SLOT(updateActions()));
+    connect(d->partWidget->fileView(), SIGNAL(currentElementChanged(QSharedPointer<Element>,File*)), this, SLOT(updateActions()));
 
-    d->editor->addAction(d->elementFindPDFAction);
-    d->editor->addAction(d->entryApplyDefaultFormatString);
+    d->partWidget->fileView()->addAction(d->elementFindPDFAction);
+    d->partWidget->fileView()->addAction(d->entryApplyDefaultFormatString);
 
-    d->colorLabelContextMenu = new ColorLabelContextMenu(d->editor);
+    d->colorLabelContextMenu = new ColorLabelContextMenu(d->partWidget->fileView());
     d->colorLabelContextMenuAction = actionCollection()->addAction(QLatin1String("entry_colorlabel"), d->colorLabelContextMenu->menuAction());
 
     setXMLFile(RCFileName);
 
-    d->findDuplicatesUI = new FindDuplicatesUI(this, d->editor);
-    d->lyx = new LyX(this, d->editor);
+    d->findDuplicatesUI = new FindDuplicatesUI(this, d->partWidget->fileView());
+    d->lyx = new LyX(this, d->partWidget->fileView());
 
     updateActions();
     fitActionSettings();
@@ -758,9 +745,9 @@ void KBibTeXPart::elementViewDocumentMenu(QObject *obj)
 
 void KBibTeXPart::elementFindPDF()
 {
-    QModelIndexList mil = d->editor->selectionModel()->selectedRows();
+    QModelIndexList mil = d->partWidget->fileView()->selectionModel()->selectedRows();
     if (mil.count() == 1) {
-        QSharedPointer<Entry> entry = d->editor->fileModel()->element(d->editor->sortFilterProxyModel()->mapToSource(*mil.constBegin()).row()).dynamicCast<Entry>();
+        QSharedPointer<Entry> entry = d->partWidget->fileView()->fileModel()->element(d->partWidget->fileView()->sortFilterProxyModel()->mapToSource(*mil.constBegin()).row()).dynamicCast<Entry>();
         if (!entry.isNull())
             FindPDFUI::interactiveFindPDF(*entry, *d->bibTeXFile, widget());
     }
@@ -768,9 +755,9 @@ void KBibTeXPart::elementFindPDF()
 
 void KBibTeXPart::applyDefaultFormatString()
 {
-    QModelIndexList mil = d->editor->selectionModel()->selectedRows();
+    QModelIndexList mil = d->partWidget->fileView()->selectionModel()->selectedRows();
     foreach(const QModelIndex &index, mil) {
-        QSharedPointer<Entry> entry = d->editor->fileModel()->element(d->editor->sortFilterProxyModel()->mapToSource(index).row()).dynamicCast<Entry>();
+        QSharedPointer<Entry> entry = d->partWidget->fileView()->fileModel()->element(d->partWidget->fileView()->sortFilterProxyModel()->mapToSource(index).row()).dynamicCast<Entry>();
         if (!entry.isNull()) {
             static IdSuggestions idSuggestions;
             bool success = idSuggestions.applyDefaultFormatId(*entry.data());
@@ -816,9 +803,9 @@ void KBibTeXPart::newEntryTriggered()
 {
     QSharedPointer<Entry> newEntry = QSharedPointer<Entry>(new Entry(QLatin1String("Article"), d->findUnusedId()));
     d->model->insertRow(newEntry, d->model->rowCount());
-    d->editor->setSelectedElement(newEntry);
-    if (d->editor->editElement(newEntry))
-        d->editor->scrollToBottom(); // FIXME always correct behaviour?
+    d->partWidget->fileView()->setSelectedElement(newEntry);
+    if (d->partWidget->fileView()->editElement(newEntry))
+        d->partWidget->fileView()->scrollToBottom(); // FIXME always correct behaviour?
     else {
         /// Editing this new element was cancelled,
         /// therefore remove it again
@@ -830,9 +817,9 @@ void KBibTeXPart::newMacroTriggered()
 {
     QSharedPointer<Macro> newMacro = QSharedPointer<Macro>(new Macro(d->findUnusedId()));
     d->model->insertRow(newMacro, d->model->rowCount());
-    d->editor->setSelectedElement(newMacro);
-    if (d->editor->editElement(newMacro))
-        d->editor->scrollToBottom(); // FIXME always correct behaviour?
+    d->partWidget->fileView()->setSelectedElement(newMacro);
+    if (d->partWidget->fileView()->editElement(newMacro))
+        d->partWidget->fileView()->scrollToBottom(); // FIXME always correct behaviour?
     else {
         /// Editing this new element was cancelled,
         /// therefore remove it again
@@ -844,9 +831,9 @@ void KBibTeXPart::newPreambleTriggered()
 {
     QSharedPointer<Preamble> newPreamble = QSharedPointer<Preamble>(new Preamble());
     d->model->insertRow(newPreamble, d->model->rowCount());
-    d->editor->setSelectedElement(newPreamble);
-    if (d->editor->editElement(newPreamble))
-        d->editor->scrollToBottom(); // FIXME always correct behaviour?
+    d->partWidget->fileView()->setSelectedElement(newPreamble);
+    if (d->partWidget->fileView()->editElement(newPreamble))
+        d->partWidget->fileView()->scrollToBottom(); // FIXME always correct behaviour?
     else {
         /// Editing this new element was cancelled,
         /// therefore remove it again
@@ -858,9 +845,9 @@ void KBibTeXPart::newCommentTriggered()
 {
     QSharedPointer<Comment> newComment = QSharedPointer<Comment>(new Comment());
     d->model->insertRow(newComment, d->model->rowCount());
-    d->editor->setSelectedElement(newComment);
-    if (d->editor->editElement(newComment))
-        d->editor->scrollToBottom(); // FIXME always correct behaviour?
+    d->partWidget->fileView()->setSelectedElement(newComment);
+    if (d->partWidget->fileView()->editElement(newComment))
+        d->partWidget->fileView()->scrollToBottom(); // FIXME always correct behaviour?
     else {
         /// Editing this new element was cancelled,
         /// therefore remove it again
@@ -870,7 +857,7 @@ void KBibTeXPart::newCommentTriggered()
 
 void KBibTeXPart::updateActions()
 {
-    bool emptySelection = d->editor->selectedElements().isEmpty();
+    bool emptySelection = d->partWidget->fileView()->selectedElements().isEmpty();
     d->elementEditAction->setEnabled(!emptySelection);
     d->editCopyAction->setEnabled(!emptySelection);
     d->editCopyReferencesAction->setEnabled(!emptySelection);
@@ -891,10 +878,10 @@ void KBibTeXPart::updateActions()
 
     /// update list of references which can be sent to LyX
     QStringList references;
-    if (d->editor->selectionModel() != NULL) {
-        QModelIndexList mil = d->editor->selectionModel()->selectedRows();
+    if (d->partWidget->fileView()->selectionModel() != NULL) {
+        QModelIndexList mil = d->partWidget->fileView()->selectionModel()->selectedRows();
         for (QModelIndexList::ConstIterator it = mil.constBegin(); it != mil.constEnd(); ++it) {
-            QSharedPointer<Entry> entry = d->editor->fileModel()->element(d->editor->sortFilterProxyModel()->mapToSource(*it).row()).dynamicCast<Entry>();
+            QSharedPointer<Entry> entry = d->partWidget->fileView()->fileModel()->element(d->partWidget->fileView()->sortFilterProxyModel()->mapToSource(*it).row()).dynamicCast<Entry>();
             if (!entry.isNull())
                 references << entry->id();
         }

src/parts/part.h
@@ -20,8 +20,10 @@
 
 #include <kparts/part.h>
 
+#include <QObject>
+
 #include "notificationhub.h"
-#include "fileview.h"
+#include "partwidget.h"
 
 class KBibTeXPart : public KParts::ReadWritePart, private NotificationListener
 {

src/program/mdiwidget.cpp
@@ -37,6 +37,7 @@
 #include <KSharedConfig>
 
 #include "kbibtexnamespace.h"
+#include "partwidget.h"
 
 class LRUItemModel : public QAbstractItemModel
 {
@@ -253,7 +254,7 @@ void MDIWidget::setFile(OpenFileInfo *openFileInfo, KService::Ptr servicePtr)
     }
 
     if (indexOf(widget) >= 0) {
-        oldEditor = qobject_cast<FileView *>(currentWidget());
+        oldEditor = qobject_cast<PartWidget *>(currentWidget())->fileView();
         hasChanged = widget != currentWidget();
     } else if (openFileInfo != NULL) {
         addWidget(widget);
@@ -263,7 +264,7 @@ void MDIWidget::setFile(OpenFileInfo *openFileInfo, KService::Ptr servicePtr)
     d->currentFile = openFileInfo;
 
     if (hasChanged) {
-        FileView *newEditor = qobject_cast<FileView *>(widget);
+        FileView *newEditor = qobject_cast<PartWidget *>(widget)->fileView();
         emit activePartChanged(part);
         emit documentSwitch(oldEditor, newEditor);
     }
@@ -281,7 +282,7 @@ void MDIWidget::setFile(OpenFileInfo *openFileInfo, KService::Ptr servicePtr)
 FileView *MDIWidget::fileView()
 {
     OpenFileInfo *ofi = d->ofim->currentFile();
-    return qobject_cast<FileView *>(ofi->part(this)->widget());
+    return qobject_cast<PartWidget *>(ofi->part(this)->widget())->fileView();
 }
 
 OpenFileInfo *MDIWidget::currentFile()