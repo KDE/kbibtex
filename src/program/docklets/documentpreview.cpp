ab77c34d59c5174beb99dd632939ff709739d5b7
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2015-03-28 18:26:18 +0100

    Removing ifdef for KDE <4.4

--- src/program/docklets/documentpreview.cpp
+++ src/program/docklets/documentpreview.cpp
@@ -599,11 +599,7 @@ void DocumentPreview::statFinished(KJob *kjob)
     KIO::StatJob *job = static_cast<KIO::StatJob *>(kjob);
     d->runningJobs.removeOne(job);
     if (!job->error()) {
-#if KDE_IS_VERSION(4, 4, 0)
         const KUrl url = job->mostLocalUrl();
-#else // KDE_IS_VERSION
-        const KUrl url = job->url();
-#endif // KDE_IS_VERSION
         DocumentPreviewPrivate::UrlInfo urlInfo = d->urlMetaInfo(url);
         setCursor(d->runningJobs.isEmpty() ? Qt::ArrowCursor : Qt::BusyCursor);
         d->addUrl(urlInfo);
