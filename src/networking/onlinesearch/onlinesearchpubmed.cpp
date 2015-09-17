828f71ed533e732757b82305ba3bbc0abb84eac3
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2015-09-17 09:12:14 +0200

    Correcting choke timeout
    The timeout for repetitive searches in PubMed was written
    under the assumption that the used time function from QDateTime
    would use milliseconds. However, as the function operates on
    seconds, the timeout eventually became about one week instead
    of 10 seconds.

--- src/networking/onlinesearch/onlinesearchpubmed.cpp
+++ src/networking/onlinesearch/onlinesearchpubmed.cpp
@@ -31,8 +31,8 @@
 #include "internalnetworkaccessmanager.h"
 
 const int OnlineSearchPubMed::maxNumResults = 25;
-const qint64 OnlineSearchPubMed::queryChokeTimeout = 10 * 1000; /// 10 seconds
-qint64 OnlineSearchPubMed::lastQueryEpoch = 0;
+const uint OnlineSearchPubMed::queryChokeTimeout = 10; /// 10 seconds
+uint OnlineSearchPubMed::lastQueryEpoch = 0;
 
 class OnlineSearchPubMed::OnlineSearchPubMedPrivate
 {
@@ -147,7 +147,7 @@ void OnlineSearchPubMed::startSearch(const QMap<QString, QString> &query, int nu
     numResults = qMin(maxNumResults, numResults);
     /// enforcing choke on number of searchs per time
     if (QDateTime::currentDateTime().toTime_t() - lastQueryEpoch < queryChokeTimeout) {
-        kDebug() << "Too many search queries per time; choke enforces pause of" << (queryChokeTimeout / 1000) << "seconds between queries";
+        kDebug() << "Too many search queries per time; choke enforces pause of" << queryChokeTimeout << "seconds between queries";
         delayedStoppedSearch(resultNoError);
         return;
     }
