e389346fc5abe0e7ed30f1839ab51ce03a69d2e5
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2015-11-02 22:46:20 +0100

    Initializing variables as early as possible

--- src/gui/element/elementeditor.cpp
+++ src/gui/element/elementeditor.cpp
@@ -75,6 +75,11 @@ public:
 
     ElementEditorPrivate(bool scrollable, ElementEditor *parent)
             : file(NULL), p(parent), previousWidget(NULL), config(KSharedConfig::openConfig(QLatin1String("kbibtexrc"))), elementChanged(false), elementUnapplied(false) {
+        internalEntry = QSharedPointer<Entry>();
+        internalMacro = QSharedPointer<Macro>();
+        internalComment = QSharedPointer<Comment>();
+        internalPreamble = QSharedPointer<Preamble>();
+
         createGUI(scrollable);
     }
 
@@ -312,10 +317,6 @@ public:
             (*it)->setModified(false);
         }
 
-        internalEntry = QSharedPointer<Entry>();
-        internalMacro = QSharedPointer<Macro>();
-        internalComment = QSharedPointer<Comment>();
-        internalPreamble = QSharedPointer<Preamble>();
         QSharedPointer<const Entry> e = element.dynamicCast<const Entry>();
         if (!e.isNull())
             internalEntry = QSharedPointer<Entry>(new Entry(*e.data()));
