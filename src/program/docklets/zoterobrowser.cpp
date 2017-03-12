8cc77ba12521d9890584a74793b15d77f44cf269
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2017-03-12 14:09:28 +0100

    Showing search results docklet on data returned from Zotero
    Fixing a minor issue as reported in comment 6 of
    KDE bug report 377401:
    
    "However, the search results panel doesn't open when I
    double click on the zotero library. It only shows
    results if I did an online search before and when the
    search panel is opened. If I close the search panel
    it doesn't appear when double clicking the library."
    
    CCBUG: 377401

--- src/program/docklets/zoterobrowser.cpp
+++ src/program/docklets/zoterobrowser.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2014 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2017 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -284,6 +284,7 @@ void ZoteroBrowser::tagDoubleClicked(const QModelIndex &index)
 void ZoteroBrowser::showItem(QSharedPointer<Element> e)
 {
     d->searchResults->insertElement(e);
+    emit itemToShow();
 }
 
 void ZoteroBrowser::reenableWidget()
