49c76152a1765581bb0ba8455349d94bc7c8b7d9
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2017-05-30 21:48:22 +0200

    Updating date in copyright statement

--- src/program/program.cpp
+++ src/program/program.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
     KLocalizedString::setApplicationDomain("kbibtex");
 
-    KAboutData aboutData(QStringLiteral("kbibtex"), i18n("KBibTeX"), QLatin1String(versionNumber), i18n("A BibTeX editor by KDE"), KAboutLicense::GPL_V2, i18n("Copyright 2004-2015 Thomas Fischer"), QString(), QStringLiteral("http://home.gna.org/kbibtex/"));
+    KAboutData aboutData(QStringLiteral("kbibtex"), i18n("KBibTeX"), QLatin1String(versionNumber), i18n("A BibTeX editor by KDE"), KAboutLicense::GPL_V2, i18n("Copyright 2004-2017 Thomas Fischer"), QString(), QStringLiteral("http://home.gna.org/kbibtex/"));
 
     aboutData.setOrganizationDomain(QByteArray("kde.org"));
     aboutData.setDesktopFileName(QStringLiteral("org.kde.kbibtex"));
