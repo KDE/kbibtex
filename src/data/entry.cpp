dabeb76f0ca1fa7ad1fd645a22b76deef3a86bb2
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2017-06-18 21:11:53 +0200

    Skip crossref resolution if non is set

--- src/data/entry.cpp
+++ src/data/entry.cpp
@@ -199,6 +199,9 @@ Entry *Entry::resolveCrossref(const Entry &original, const File *bibTeXfile)
         return result;
 
     const QString crossRef = PlainTextValue::text(original.value(ftCrossRef));
+    if (crossRef.isEmpty())
+        return result;
+
     const QSharedPointer<Entry> crossRefEntry = bibTeXfile->containsKey(crossRef, File::etEntry).dynamicCast<Entry>();
     if (!crossRefEntry.isNull()) {
         /// copy all fields from crossref'ed entry to new entry which do not (yet) exist in the new entry
