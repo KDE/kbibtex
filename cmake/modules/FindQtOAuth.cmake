1a360a63883b4b2cdac2f61912845c65c3a9c3bd
Thomas Fischer <thomas.fischer@his.se>
2018-01-17 11:40:06 +0100

    First check for qtoauth-qt5, only then for qtoauth
    During the cmake phase, KBibTeX will look for a Qt-based OAuth library.
    Previously, it used PkgConfig to identify a library called 'qtoauth',
    assuming this would be a Qt5-based library as it is the case for
    Linux distributions such as Gentoo Linux or Arch Linux.
    
    However, other distributions such as Fedora maintain both a Qt4-based
    and a Qt5-based variant of this library under the names 'qtoauth' and
    'qtoauth-qt5', respectively. Thus, simply checking for a library called
    'qtoauth' would result in linking to a Qt4-based library and in issues
    like reported in bug 387638.
    
    This commit introduces an advanced check, where first a test for
    'qtoauth-qt5' is done. If this probe fails (like on Gentoo), a test
    for 'qtoauth' is done.
    Eventually, the correct Qt5-based QtOAuth library will be identified.
    
    BUG: 387638

--- cmake/modules/FindQtOAuth.cmake
+++ cmake/modules/FindQtOAuth.cmake
@@ -21,13 +21,25 @@ if (QTOAUTH_INCLUDE_DIR AND QTOAUTH_LIBRARY)
 else (QTOAUTH_INCLUDE_DIR AND QTOAUTH_LIBRARY)
   if (NOT WIN32)
     find_package(PkgConfig)
-    pkg_check_modules(PC_QTOAUTH QUIET qoauth)
+    pkg_check_modules(PC_QTOAUTH QUIET qoauth-qt5)
+    if ("${PC_QTOAUTH_INCLUDEDIR}" STREQUAL "" AND "${PC_QTOAUTH_INCLUDE_DIRS}" STREQUAL "")
+      # Testing explicitly for Qt5 variant of qoauth failed,
+      # maybe testing for a generic qoauth variant will succeed?
+      pkg_check_modules(PC_QTOAUTH QUIET qoauth)
+    endif ("${PC_QTOAUTH_INCLUDEDIR}" STREQUAL "" AND "${PC_QTOAUTH_INCLUDE_DIRS}" STREQUAL "")
     set(QTOAUTH_DEFINITIONS ${PC_QTOAUTH_CFLAGS_OTHER})
   endif(NOT WIN32)
 
-  find_library(QTOAUTH_LIBRARY NAMES qoauth
+  find_library(QTOAUTH_LIBRARY NAMES qoauth-qt5
     HINTS ${PC_QTOAUTH_LIBDIR} ${PC_QTOAUTH_LIBRARY_DIRS}
   )
+  if ("${QTOAUTH_LIBRARY}" STREQUAL "QTOAUTH_LIBRARY-NOTFOUND")
+    # Similar problem as above: maybe the library is not
+    # called '*qoauth-qt5*', but just '*qoauth*'
+    find_library(QTOAUTH_LIBRARY NAMES qoauth
+      HINTS ${PC_QTOAUTH_LIBDIR} ${PC_QTOAUTH_LIBRARY_DIRS}
+    )
+  endif ("${QTOAUTH_LIBRARY}" STREQUAL "QTOAUTH_LIBRARY-NOTFOUND")
 
   find_path(QTOAUTH_INCLUDE_DIR QtOAuth/interface.h
     HINTS ${PC_QTOAUTH_INCLUDEDIR} ${PC_QTOAUTH_INCLUDE_DIRS}
