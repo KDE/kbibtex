4921e971aeca4186e1a3f35307612bb7653b6fa1
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2018-02-14 22:44:09 +0100

    Refactoring generation of headers for version numbers
    Instead of manually modifying the source code upon release to
    contain a version number, the version number is taken from the
    main CMakeLists.txt file: ecm_setup_version(..) generates a
    header file containing this global version number for inclusion
    in C++ source code.
    
    However, the 'Git revision to header file' code is still used,
    though heavily refactored.
    
    Both the global version number from the main CMakeLists.txt file
    and the Git revision will be evaluated to compute the version
    number as shown to the user:
    - If there is a .git directory and the Git revision can be
      determined, use it as the 'main' version number.
      If there is a CMakeLists.txt-based version number show it
      as additional information to the user ("... near 0.7.90").
    - If there is no .git directory, use only the version number
      from the main CMakeLists.txt file.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2017 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2018 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -22,13 +22,14 @@
 #include <KLocalizedString>
 
 #include "kbibtextest.h"
-#include "version.h"
+#include "kbibtex-version.h"
+#include "kbibtex-git-info.h"
 
 int main(int argc, char *argv[])
 {
     QApplication programCore(argc, argv);
 
-    KAboutData aboutData(QStringLiteral("kbibtextest"), i18n("KBibTeX Test"), QLatin1String(versionNumber), i18n("A BibTeX editor by KDE"), KAboutLicense::GPL_V2, i18n("Copyright 2004-2017 Thomas Fischer"), QString(), QStringLiteral("https://userbase.kde.org/KBibTeX"));
+    KAboutData aboutData(QStringLiteral("kbibtextest"), i18n("KBibTeX Test"), strlen(KBIBTEX_GIT_INFO_STRING) > 0 ? QLatin1String(KBIBTEX_GIT_INFO_STRING ", near " KBIBTEX_VERSION_STRING) : QLatin1String(KBIBTEX_VERSION_STRING), i18n("A BibTeX editor by KDE"), KAboutLicense::GPL_V2, i18n("Copyright 2004-2017 Thomas Fischer"), QString(), QStringLiteral("https://userbase.kde.org/KBibTeX"));
     aboutData.addAuthor(i18n("Thomas Fischer"), i18n("Maintainer"), QStringLiteral("fischer@unix-ag.uni-kl.de"));
     KAboutData::setApplicationData(aboutData);
 
