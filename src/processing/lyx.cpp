KDE/kbibtex

commit 9a34e236938b83a878385c6f6116e159aa3e6043

LyX pipe autodetection depends on QT_LSTAT macro
Detection of the LyX pipe autodetection depends on the availability
of the QT_LSTAT macro which wraps around lstat or lstat64 on POSIX/
Unix/Linux platforms but is not available on Windows.

src/processing/lyx.cpp
@@ -58,12 +58,14 @@ public:
         if (!group.readEntry(keyUseAutomaticLyXPipeDetection, defaultUseAutomaticLyXPipeDetection))
             result = group.readEntry(keyLyXPipePath, defaultLyXPipePath);
 
+#ifdef QT_LSTAT
         /// Check if the result so far is empty. This means that
         /// either automatic detection is enabled or the path in
         /// the configuration is empty/invalid. Proceed with
         /// automatic detection in this case.
         if (result.isEmpty())
             result = LyX::guessLyXPipeLocation();
+#endif // QT_LSTAT
 
         /// Finally, even if automatic detection was preferred by the user,
         /// still check configuration for a path if automatic detection failed
@@ -77,12 +79,9 @@ public:
 
 const QString LyX::keyUseAutomaticLyXPipeDetection = QStringLiteral("UseAutomaticLyXPipeDetection");
 const QString LyX::keyLyXPipePath = QStringLiteral("LyXPipePath");
-const bool LyX::defaultUseAutomaticLyXPipeDetection =
-#ifdef Q_WS_WIN
-    false; /// Windows is not supported yet
-#else // Q_WS_WIN
-    true;
-#endif // Q_WS_WIN
+#ifdef QT_LSTAT
+const bool LyX::defaultUseAutomaticLyXPipeDetection = true;
+#endif // QT_LSTAT
 const QString LyX::defaultLyXPipePath = QString();
 const QString LyX::configGroupName = QStringLiteral("LyXPipe");
 
@@ -140,6 +139,7 @@ void LyX::sendReferenceToLyX()
     pipe.close();
 }
 
+#ifdef QT_LSTAT
 QString LyX::guessLyXPipeLocation()
 {
     QT_STATBUF statBuffer;
@@ -175,3 +175,4 @@ QString LyX::guessLyXPipeLocation()
 
     return result;
 }
+#endif // QT_LSTAT

src/processing/lyx.h
@@ -21,6 +21,7 @@
 #include "kbibtexproc_export.h"
 
 #include <QObject>
+#include <qplatformdefs.h>
 
 namespace KParts
 {
@@ -38,7 +39,9 @@ class KBIBTEXPROC_EXPORT LyX: public QObject
 public:
     static const QString keyUseAutomaticLyXPipeDetection;
     static const QString keyLyXPipePath;
+#ifdef QT_LSTAT
     static const bool defaultUseAutomaticLyXPipeDetection;
+#endif // QT_LSTAT
     static const QString defaultLyXPipePath;
     static const QString configGroupName;
 
@@ -47,7 +50,9 @@ public:
 
     void setReferences(const QStringList &references);
 
+#ifdef QT_LSTAT
     static QString guessLyXPipeLocation();
+#endif // QT_LSTAT
 
 private slots:
     void sendReferenceToLyX();

src/gui/preferences/settingsfileexporterwidget.cpp
@@ -21,6 +21,7 @@
 #include <QCheckBox>
 #include <QSpinBox>
 #include <QPushButton>
+#include <qplatformdefs.h>
 
 #include <KLocalizedString>
 #include <KSharedConfig>
@@ -47,12 +48,16 @@ private:
     KSharedConfigPtr config;
 
 public:
+#ifdef QT_LSTAT
     QCheckBox *checkboxUseAutomaticLyXPipeDetection;
+#endif // QT_LSTAT
     KComboBox *comboBoxBackupScope;
     QSpinBox *spinboxNumberOfBackups;
 
     KUrlRequester *lineeditLyXPipePath;
-    QString lastUserInput;
+#ifdef QT_LSTAT
+    QString lastUserInputLyXPipePath;
+#endif // QT_LSTAT
 
     SettingsFileExporterWidgetPrivate(SettingsFileExporterWidget *parent)
             : p(parent), config(KSharedConfig::openConfig(QStringLiteral("kbibtexrc"))) {
@@ -70,9 +75,13 @@ public:
         spinboxNumberOfBackups->setValue(qMax(0, qMin(spinboxNumberOfBackups->maximum(), configGroup.readEntry(Preferences::keyNumberOfBackups, Preferences::defaultNumberOfBackups))));
 
         KConfigGroup configGroupLyX(config, LyX::configGroupName);
+#ifndef QT_LSTAT
+        lineeditLyXPipePath->setText(configGroupLyX.readEntry(LyX::keyLyXPipePath, LyX::defaultLyXPipePath));
+#else // QT_LSTAT
         checkboxUseAutomaticLyXPipeDetection->setChecked(configGroupLyX.readEntry(LyX::keyUseAutomaticLyXPipeDetection, LyX::defaultUseAutomaticLyXPipeDetection));
-        lastUserInput = configGroupLyX.readEntry(LyX::keyLyXPipePath, LyX::defaultLyXPipePath);
+        lastUserInputLyXPipePath = configGroupLyX.readEntry(LyX::keyLyXPipePath, LyX::defaultLyXPipePath);
         p->automaticLyXDetectionToggled(checkboxUseAutomaticLyXPipeDetection->isChecked());
+#endif // QT_LSTAT
     }
 
     void saveState() {
@@ -84,8 +93,12 @@ public:
         configGroup.writeEntry(Preferences::keyNumberOfBackups, spinboxNumberOfBackups->value());
 
         KConfigGroup configGroupLyX(config, LyX::configGroupName);
+#ifndef QT_LSTAT
+        configGroupLyX.writeEntry(LyX::keyLyXPipePath, lineeditLyXPipePath->text());
+#else // QT_LSTAT
         configGroupLyX.writeEntry(LyX::keyUseAutomaticLyXPipeDetection, checkboxUseAutomaticLyXPipeDetection->isChecked());
-        configGroupLyX.writeEntry(LyX::keyLyXPipePath, checkboxUseAutomaticLyXPipeDetection->isChecked() ? lastUserInput : lineeditLyXPipePath->text());
+        configGroupLyX.writeEntry(LyX::keyLyXPipePath, checkboxUseAutomaticLyXPipeDetection->isChecked() ? lastUserInputLyXPipePath : lineeditLyXPipePath->text());
+#endif // QT_LSTAT
 
         config->sync();
     }
@@ -98,9 +111,13 @@ public:
         comboBoxBackupScope->setCurrentIndex(index);
         spinboxNumberOfBackups->setValue(qMax(0, qMin(spinboxNumberOfBackups->maximum(), Preferences::defaultNumberOfBackups)));
 
+#ifndef QT_LSTAT
+        const QString pipe = LyX::defaultLyXPipePath;
+#else // QT_LSTAT
         checkboxUseAutomaticLyXPipeDetection->setChecked(LyX::defaultUseAutomaticLyXPipeDetection);
         QString pipe = LyX::guessLyXPipeLocation();
         if (pipe.isEmpty()) pipe = LyX::defaultLyXPipePath;
+#endif // QT_LSTAT
         lineeditLyXPipePath->setText(pipe);
     }
 
@@ -119,10 +136,12 @@ public:
         comboBoxCopyReferenceCmd->setModel(itim);
         connect(comboBoxCopyReferenceCmd, static_cast<void(KComboBox::*)(int)>(&KComboBox::currentIndexChanged), p, &SettingsFileExporterWidget::changed);
 
+#ifdef QT_LSTAT
         checkboxUseAutomaticLyXPipeDetection = new QCheckBox(QStringLiteral(""), p);
         layout->addRow(i18n("Detect LyX pipe automatically:"), checkboxUseAutomaticLyXPipeDetection);
         connect(checkboxUseAutomaticLyXPipeDetection, &QCheckBox::toggled, p, &SettingsFileExporterWidget::changed);
         connect(checkboxUseAutomaticLyXPipeDetection, &QCheckBox::toggled, p, &SettingsFileExporterWidget::automaticLyXDetectionToggled);
+#endif // QT_LSTAT
 
         lineeditLyXPipePath = new KUrlRequester(p);
         layout->addRow(i18n("Manually specified LyX pipe:"), lineeditLyXPipePath);
@@ -186,15 +205,17 @@ void SettingsFileExporterWidget::resetToDefaults()
     d->resetToDefaults();
 }
 
+#ifdef QT_LSTAT
 void SettingsFileExporterWidget::automaticLyXDetectionToggled(bool isChecked)
 {
     d->lineeditLyXPipePath->setEnabled(!isChecked);
     if (isChecked) {
-        d->lastUserInput = d->lineeditLyXPipePath->text();
+        d->lastUserInputLyXPipePath = d->lineeditLyXPipePath->text();
         d->lineeditLyXPipePath->setText(LyX::guessLyXPipeLocation());
     } else
-        d->lineeditLyXPipePath->setText(d->lastUserInput);
+        d->lineeditLyXPipePath->setText(d->lastUserInputLyXPipePath);
 }
+#endif // QT_LSTAT
 
 void SettingsFileExporterWidget::updateGUI()
 {

src/gui/preferences/settingsfileexporterwidget.h
@@ -20,6 +20,8 @@
 
 #include <kbibtexgui_export.h>
 
+#include <qplatformdefs.h>
+
 #include "settingsabstractwidget.h"
 
 /**
@@ -40,7 +42,9 @@ public slots:
     void loadState();
     void saveState();
     void resetToDefaults();
+#ifdef QT_LSTAT
     void automaticLyXDetectionToggled(bool);
+#endif // QT_LSTAT
 
 private slots:
     void updateGUI();

commit 37f4cb73854c302989f4b910710b876d2ce44a9f

Removing unnecessary #ifdef QT_LSTAT
Variable 'defaultUseAutomaticLyXPipeDetection' is used even if QT_LSTAT is not set,
as such should this variable not be compile-time depending on QT_LSTAT.
Fixing issue as introduced in commit 9a34e236938b8.

src/processing/lyx.cpp
@@ -77,9 +77,7 @@ public:
 
 const QString LyX::keyUseAutomaticLyXPipeDetection = QStringLiteral("UseAutomaticLyXPipeDetection");
 const QString LyX::keyLyXPipePath = QStringLiteral("LyXPipePath");
-#ifdef QT_LSTAT
 const bool LyX::defaultUseAutomaticLyXPipeDetection = true;
-#endif // QT_LSTAT
 const QString LyX::defaultLyXPipePath = QString();
 const QString LyX::configGroupName = QStringLiteral("LyXPipe");
 

src/processing/lyx.h
@@ -39,9 +39,7 @@ class KBIBTEXPROC_EXPORT LyX: public QObject
 public:
     static const QString keyUseAutomaticLyXPipeDetection;
     static const QString keyLyXPipePath;
-#ifdef QT_LSTAT
     static const bool defaultUseAutomaticLyXPipeDetection;
-#endif // QT_LSTAT
     static const QString defaultLyXPipePath;
     static const QString configGroupName;
 