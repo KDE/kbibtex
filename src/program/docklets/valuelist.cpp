0b73a4446b2d913b0862085f5771dd8ad102ad2b
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2018-08-09 16:16:12 +0200

    Replacing 'setClearButtonShown' with 'setClearButtonEnabled'
    Replacing KLineEdit's deprecated 'setClearButtonShown' with
    'setClearButtonEnabled' as documented at:
      https://community.kde.org/Frameworks/Porting_Notes#General_2

--- src/program/docklets/valuelist.cpp
+++ src/program/docklets/valuelist.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2017 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2018 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -87,7 +87,7 @@ public:
 
         lineeditFilter = new KLineEdit(p);
         layout->addWidget(lineeditFilter);
-        lineeditFilter->setClearButtonShown(true);
+        lineeditFilter->setClearButtonEnabled(true);
         lineeditFilter->setPlaceholderText(i18n("Filter value list"));
 
         treeviewFieldValues = new QTreeView(p);
