453e342661e783c1544a8632cc8dcaef1938f243
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2018-12-31 19:13:19 +0100

    Using HTTPS instead of HTTP for Ingenta Connect

--- src/networking/onlinesearch/onlinesearchingentaconnect.cpp
+++ src/networking/onlinesearch/onlinesearchingentaconnect.cpp
@@ -176,7 +176,7 @@ public:
 #endif // HAVE_QTWIDGETS
 
     OnlineSearchIngentaConnectPrivate(OnlineSearchIngentaConnect *)
-            : ingentaConnectBaseUrl(QStringLiteral("http://www.ingentaconnect.com/search?format=bib"))
+            : ingentaConnectBaseUrl(QStringLiteral("https://www.ingentaconnect.com/search?format=bib"))
 #ifdef HAVE_QTWIDGETS
         , form(nullptr)
 #endif // HAVE_QTWIDGETS
@@ -376,7 +376,7 @@ OnlineSearchQueryFormAbstract *OnlineSearchIngentaConnect::customWidget(QWidget 
 
 QUrl OnlineSearchIngentaConnect::homepage() const
 {
-    return QUrl(QStringLiteral("http://www.ingentaconnect.com/"));
+    return QUrl(QStringLiteral("https://www.ingentaconnect.com/"));
 }
 
 void OnlineSearchIngentaConnect::downloadDone()
