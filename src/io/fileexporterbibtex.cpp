KDE/kbibtex

commit fa2f61c53e39da37edf7f2d538a2d1d4853e69b2

Refactoring 'convertToPlainAscii' from EncoderLaTeX to Encoder
Refactoring as well related function 'containsOnlyAscii'.
This refactoring is motivated by the fact that those functions
are not specific to LaTeX encoding and thus should be put into
a more general place.

src/io/fileexporterbibtex.cpp
@@ -32,7 +32,7 @@
 #include "preamble.h"
 #include "value.h"
 #include "comment.h"
-#include "encoderlatex.h"
+#include "encoder.h"
 #include "bibtexentries.h"
 #include "bibtexfields.h"
 #include "textencoder.h"
@@ -121,13 +121,11 @@ public:
     }
 
     bool writeEntry(QIODevice *iodevice, const Entry &entry) {
-        const EncoderLaTeX &laTeXEncoder = EncoderLaTeX::instance();
-
         /// write start of a entry (entry type and id) in plain ASCII
         iodevice->putChar('@');
         iodevice->write(BibTeXEntries::instance().format(entry.type(), keywordCasing).toLatin1().data());
         iodevice->putChar('{');
-        iodevice->write(laTeXEncoder.convertToPlainAscii(entry.id()).toLatin1());
+        iodevice->write(Encoder::instance().convertToPlainAscii(entry.id()).toLatin1());
 
         for (Entry::ConstIterator it = entry.constBegin(); it != entry.constEnd(); ++it) {
             const QString key = it.key();
@@ -153,7 +151,7 @@ public:
             iodevice->putChar(',');
             iodevice->putChar('\n');
             iodevice->putChar('\t');
-            iodevice->write(laTeXEncoder.convertToPlainAscii(BibTeXFields::instance().format(key, keywordCasing)).toLatin1());
+            iodevice->write(Encoder::instance().convertToPlainAscii(BibTeXFields::instance().format(key, keywordCasing)).toLatin1());
             iodevice->putChar(' ');
             iodevice->putChar('=');
             iodevice->putChar(' ');
@@ -542,8 +540,8 @@ QString FileExporterBibTeX::valueToBibTeX(const Value &value, const QString &key
 
 QString FileExporterBibTeX::applyEncoder(const QString &input, UseLaTeXEncoding useLaTeXEncoding) const {
     switch (useLaTeXEncoding) {
-    case leLaTeX: return EncoderLaTeX::instance().encode(input, Encoder::TargetEncodingASCII);
-    case leUTF8: return EncoderLaTeX::instance().encode(input, Encoder::TargetEncodingUTF8);
+    case leLaTeX: return Encoder::instance().encode(input, Encoder::TargetEncodingASCII);
+    case leUTF8: return Encoder::instance().encode(input, Encoder::TargetEncodingUTF8);
     default: return input;
     }
 }

src/io/encoder.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2017 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -17,6 +17,29 @@
 
 #include "encoder.h"
 
+#include "logging_io.h"
+
+Encoder::Encoder()
+{
+#ifdef HAVE_ICU
+    /// Create an ICU Transliterator, configured to
+    /// transliterate virtually anything into plain ASCII
+    UErrorCode uec = U_ZERO_ERROR;
+    m_trans = icu::Transliterator::createInstance("Any-Latin;Latin-ASCII", UTRANS_FORWARD, uec);
+    if (U_FAILURE(uec)) {
+        qCWarning(LOG_KBIBTEX_IO) << "Error creating an ICU Transliterator instance: " << u_errorName(uec);
+        if (m_trans != nullptr) delete m_trans;
+        m_trans = nullptr;
+    }
+#endif // HAVE_ICU
+}
+
+const Encoder &Encoder::instance()
+{
+    static const Encoder self;
+    return self;
+}
+
 QString Encoder::decode(const QString &text) const
 {
     return text;
@@ -26,3 +49,51 @@ QString Encoder::encode(const QString &text, const TargetEncoding) const
 {
     return text;
 }
+
+#ifdef HAVE_ICU
+QString Encoder::convertToPlainAscii(const QString &ninput) const
+{
+    /// Previously, iconv's //TRANSLIT feature had been used here.
+    /// However, the transliteration is locale-specific as discussed
+    /// here:
+    /// http://taschenorakel.de/mathias/2007/11/06/iconv-transliterations/
+    /// Therefore, iconv is not an acceptable solution.
+    ///
+    /// Instead, "International Components for Unicode" (ICU) is used.
+    /// It is already a dependency for Qt, so there is no "cost" involved
+    /// in using it.
+
+    /// Preprocessing where ICU may give unexpected results otherwise
+    QString input = ninput;
+    input = input.replace(QChar(0x2013), QStringLiteral("--")).replace(QChar(0x2014), QStringLiteral("---"));
+
+    const int inputLen = input.length();
+    /// Make a copy of the input string into an array of UChar
+    UChar *uChars = new UChar[inputLen];
+    for (int i = 0; i < inputLen; ++i)
+        uChars[i] = input.at(i).unicode();
+    /// Create an ICU-specific unicode string
+    icu::UnicodeString uString = icu::UnicodeString(uChars, inputLen);
+    /// Perform the actual transliteration, modifying Unicode string
+    if (m_trans != nullptr) m_trans->transliterate(uString);
+    /// Create regular C++ string from Unicode string
+    std::string cppString;
+    uString.toUTF8String(cppString);
+    /// Clean up any mess
+    delete[] uChars;
+    /// Convert regular C++ to Qt-specific QString,
+    /// should work as cppString contains only ASCII text
+    return QString::fromStdString(cppString);
+}
+
+bool Encoder::containsOnlyAscii(const QString &ntext)
+{
+    /// Perform Canonical Decomposition followed by Canonical Composition
+    const QString text = ntext.normalized(QString::NormalizationForm_C);
+
+    for (const QChar &c : text)
+        if (c.unicode() > 127) return false;
+    return true;
+}
+
+#endif // HAVE_ICU

src/io/encoder.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2017 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -18,6 +18,14 @@
 #ifndef BIBTEXENCODER_H
 #define BIBTEXENCODER_H
 
+#ifdef HAVE_KF5
+#include "kbibtexio_export.h"
+#endif // HAVE_KF5
+
+#ifdef HAVE_ICU
+#include <unicode/translit.h>
+#endif // HAVE_ICU
+
 #include <QString>
 
 /**
@@ -26,13 +34,17 @@
  * are \"a in LaTeX and &auml; in XML.
  * @author Thomas Fischer <fischer@unix-ag.uni-kl.de>
  */
-class Encoder
+class KBIBTEXIO_EXPORT Encoder
 {
 public:
     enum TargetEncoding {TargetEncodingASCII = 0, TargetEncodingUTF8 = 1};
 
+    static const Encoder &instance();
     virtual ~Encoder() {
-        /// nothing
+#ifdef HAVE_ICU
+        if (m_trans != nullptr)
+            delete m_trans;
+#endif // HAVE_ICU
     }
 
     /**
@@ -53,6 +65,24 @@ public:
      * @return text text in external textual representation
      */
     virtual QString encode(const QString &text, const TargetEncoding targetEncoding) const;
+
+#ifdef HAVE_ICU
+    QString convertToPlainAscii(const QString &input) const;
+#else // HAVE_ICU
+    /// Dummy implementation without ICU
+    inline QString convertToPlainAscii(const QString &input) const {
+        return input;
+    }
+#endif // HAVE_ICU
+    static bool containsOnlyAscii(const QString &text);
+
+protected:
+    Encoder();
+
+private:
+#ifdef HAVE_ICU
+    icu::Transliterator *m_trans;
+#endif // HAVE_ICU
 };
 
 #endif

src/io/encoderlatex.cpp
@@ -900,19 +900,8 @@ encoderLaTeXSymbolSequences[] = {
 
 
 EncoderLaTeX::EncoderLaTeX()
+        : Encoder()
 {
-#ifdef HAVE_ICU
-    /// Create an ICU Transliterator, configured to
-    /// transliterate virtually anything into plain ASCII
-    UErrorCode uec = U_ZERO_ERROR;
-    m_trans = icu::Transliterator::createInstance("Any-Latin;Latin-ASCII", UTRANS_FORWARD, uec);
-    if (U_FAILURE(uec)) {
-        qCWarning(LOG_KBIBTEX_IO) << "Error creating an ICU Transliterator instance: " << u_errorName(uec);
-        if (m_trans != nullptr) delete m_trans;
-        m_trans = nullptr;
-    }
-#endif // HAVE_ICU
-
     /// Initialize lookup table with NULL pointers
     for (int i = 0; i < lookupTableNumModifiers; ++i) lookupTable[i] = nullptr;
 
@@ -960,11 +949,6 @@ EncoderLaTeX::~EncoderLaTeX()
     for (int i = lookupTableNumModifiers - 1; i >= 0; --i)
         if (lookupTable[i] != nullptr)
             delete lookupTable[i];
-
-#ifdef HAVE_ICU
-    if (m_trans != nullptr)
-        delete m_trans;
-#endif // HAVE_ICU
 }
 
 QString EncoderLaTeX::decode(const QString &input) const
@@ -1483,53 +1467,6 @@ QString EncoderLaTeX::encode(const QString &ninput, const TargetEncoding targetE
 
     output.squeeze();
     return output;
-}
-
-#ifdef HAVE_ICU
-QString EncoderLaTeX::convertToPlainAscii(const QString &ninput) const
-{
-    /// Previously, iconv's //TRANSLIT feature had been used here.
-    /// However, the transliteration is locale-specific as discussed
-    /// here:
-    /// http://taschenorakel.de/mathias/2007/11/06/iconv-transliterations/
-    /// Therefore, iconv is not an acceptable solution.
-    ///
-    /// Instead, "International Components for Unicode" (ICU) is used.
-    /// It is already a dependency for Qt, so there is no "cost" involved
-    /// in using it.
-
-    /// Preprocessing where ICU may give unexpected results otherwise
-    QString input = ninput;
-    input = input.replace(QChar(0x2013), QStringLiteral("--")).replace(QChar(0x2014), QStringLiteral("---"));
-
-    const int inputLen = input.length();
-    /// Make a copy of the input string into an array of UChar
-    UChar *uChars = new UChar[inputLen];
-    for (int i = 0; i < inputLen; ++i)
-        uChars[i] = input.at(i).unicode();
-    /// Create an ICU-specific unicode string
-    icu::UnicodeString uString = icu::UnicodeString(uChars, inputLen);
-    /// Perform the actual transliteration, modifying Unicode string
-    if (m_trans != nullptr) m_trans->transliterate(uString);
-    /// Create regular C++ string from Unicode string
-    std::string cppString;
-    uString.toUTF8String(cppString);
-    /// Clean up any mess
-    delete[] uChars;
-    /// Convert regular C++ to Qt-specific QString,
-    /// should work as cppString contains only ASCII text
-    return QString::fromStdString(cppString);
-}
-#endif // HAVE_ICU
-
-bool EncoderLaTeX::containsOnlyAscii(const QString &ntext)
-{
-    /// Perform Canonical Decomposition followed by Canonical Composition
-    const QString text = ntext.normalized(QString::NormalizationForm_C);
-
-    for (const QChar &c : text)
-        if (c.unicode() > 127) return false;
-    return true;
 }
 
 int EncoderLaTeX::modifierInLookupTable(const QChar modifier) const

src/io/encoderlatex.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2018 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -22,10 +22,6 @@
 #include "kbibtexio_export.h"
 #endif // HAVE_KF5
 
-#ifdef HAVE_ICU
-#include <unicode/translit.h>
-#endif // HAVE_ICU
-
 #include <QIODevice>
 
 #include "encoder.h"
@@ -41,13 +37,6 @@ class KBIBTEXIO_EXPORT EncoderLaTeX: public Encoder
 public:
     QString decode(const QString &text) const override;
     QString encode(const QString &text, const TargetEncoding targetEncoding) const override;
-#ifdef HAVE_ICU
-    QString convertToPlainAscii(const QString &input) const;
-#else // HAVE_ICU
-    /// Dummy implementation without ICU
-    inline QString convertToPlainAscii(const QString &input) const { return input; }
-#endif // HAVE_ICU
-    static bool containsOnlyAscii(const QString &text);
 
     static const EncoderLaTeX &instance();
     ~EncoderLaTeX() override;
@@ -100,10 +89,6 @@ private:
      * Return value may be an empty string.
      */
     QString readAlphaCharacters(const QString &base, int startFrom) const;
-
-#ifdef HAVE_ICU
-    icu::Transliterator *m_trans;
-#endif // HAVE_ICU
 };
 
 #endif // ENCODERLATEX_H

src/io/fileimporterbibtex.cpp
@@ -31,6 +31,7 @@
 #include "entry.h"
 #include "element.h"
 #include "value.h"
+#include "encoder.h"
 #include "encoderlatex.h"
 #include "bibtexentries.h"
 #include "bibtexfields.h"
@@ -266,9 +267,9 @@ Macro *FileImporterBibTeX::readMacroElement()
         /// Cope with empty keys,
         /// duplicates are handled further below
         key = QStringLiteral("EmptyId");
-    } else if (!EncoderLaTeX::containsOnlyAscii(key)) {
+    } else if (!Encoder::containsOnlyAscii(key)) {
         /// Try to avoid non-ascii characters in ids
-        const QString newKey = EncoderLaTeX::instance().convertToPlainAscii(key);
+        const QString newKey = Encoder::instance().convertToPlainAscii(key);
         qCWarning(LOG_KBIBTEX_IO) << "Macro key" << key << "near line" << m_lineNo << "contains non-ASCII characters, converted to" << newKey;
         emit message(SeverityWarning, QString(QStringLiteral("Macro key '%1'  near line %2 contains non-ASCII characters, converted to '%3'")).arg(key).arg(m_lineNo).arg(newKey));
         key = newKey;
@@ -381,9 +382,9 @@ Entry *FileImporterBibTeX::readEntryElement(const QString &typeString)
             emit message(SeverityWarning, QString(QStringLiteral("Entry id '%1' near line %2 contains backslashes or curly brackets, converted to '%3'")).arg(id).arg(m_lineNo).arg(newId));
             id = newId;
         }
-        if (!EncoderLaTeX::containsOnlyAscii(id)) {
+        if (!Encoder::containsOnlyAscii(id)) {
             /// Try to avoid non-ascii characters in ids
-            const QString newId = EncoderLaTeX::instance().convertToPlainAscii(id);
+            const QString newId = Encoder::instance().convertToPlainAscii(id);
             qCWarning(LOG_KBIBTEX_IO) << "Entry id" << id << "near line" << m_lineNo << "contains non-ASCII characters, converted to" << newId;
             emit message(SeverityWarning, QString(QStringLiteral("Entry id '%1' near line %2 contains non-ASCII characters, converted to '%3'")).arg(id).arg(m_lineNo).arg(newId));
             id = newId;
@@ -455,7 +456,7 @@ Entry *FileImporterBibTeX::readEntryElement(const QString &typeString)
             }
         }
         /// Try to avoid non-ascii characters in keys
-        const QString newkeyName = EncoderLaTeX::instance().convertToPlainAscii(keyName);
+        const QString newkeyName = Encoder::instance().convertToPlainAscii(keyName);
         if (newkeyName != keyName) {
             qCWarning(LOG_KBIBTEX_IO) << "Field name " << keyName << "near line" << m_lineNo << "contains non-ASCII characters, converted to" << newkeyName;
             emit message(SeverityWarning, QString(QStringLiteral("Field name '%1' near line %2 contains non-ASCII characters, converted to '%3'")).arg(keyName).arg(m_lineNo).arg(newkeyName));

src/networking/onlinesearch/onlinesearchabstract.cpp
@@ -34,7 +34,7 @@
 #include <KMessageBox>
 #endif // HAVE_KF5
 
-#include "encoderlatex.h"
+#include "encoder.h"
 #include "internalnetworkaccessmanager.h"
 #include "kbibtex.h"
 #include "logging_networking.h"
@@ -57,7 +57,7 @@ const char *OnlineSearchAbstract::httpUnsafeChars = "%:/=+$?&\0";
 #ifdef HAVE_QTWIDGETS
 QStringList OnlineSearchQueryFormAbstract::authorLastNames(const Entry &entry)
 {
-    const EncoderLaTeX &encoder = EncoderLaTeX::instance();
+    const Encoder &encoder = Encoder::instance();
     const Value v = entry[Entry::ftAuthor];
     QStringList result;
     result.reserve(v.size());

src/networking/onlinesearch/onlinesearchspringerlink.cpp
@@ -37,7 +37,7 @@
 #endif // HAVE_KF5
 
 #include "internalnetworkaccessmanager.h"
-#include "encoderlatex.h"
+#include "encoder.h"
 #include "encoderxml.h"
 #include "fileimporterbibtex.h"
 #include "xsltransform.h"
@@ -178,17 +178,17 @@ public:
 
         const QStringList titleChunks = OnlineSearchAbstract::splitRespectingQuotationMarks(form->lineEditTitle->text());
         for (const QString &titleChunk : titleChunks) {
-            queryString += QString(QStringLiteral(" title:%1")).arg(EncoderLaTeX::instance().convertToPlainAscii(titleChunk));
+            queryString += QString(QStringLiteral(" title:%1")).arg(Encoder::instance().convertToPlainAscii(titleChunk));
         }
 
         const QStringList bookTitleChunks = OnlineSearchAbstract::splitRespectingQuotationMarks(form->lineEditBookTitle->text());
         for (const QString &titleChunk : bookTitleChunks) {
-            queryString += QString(QStringLiteral(" ( journal:%1 OR book:%1 )")).arg(EncoderLaTeX::instance().convertToPlainAscii(titleChunk));
+            queryString += QString(QStringLiteral(" ( journal:%1 OR book:%1 )")).arg(Encoder::instance().convertToPlainAscii(titleChunk));
         }
 
         const QStringList authors = OnlineSearchAbstract::splitRespectingQuotationMarks(form->lineEditAuthorEditor->text());
         for (const QString &author : authors) {
-            queryString += QString(QStringLiteral(" name:%1")).arg(EncoderLaTeX::instance().convertToPlainAscii(author));
+            queryString += QString(QStringLiteral(" name:%1")).arg(Encoder::instance().convertToPlainAscii(author));
         }
 
         const QString year = form->lineEditYear->text();
@@ -211,12 +211,12 @@ public:
 
         const QStringList titleChunks = OnlineSearchAbstract::splitRespectingQuotationMarks(query[queryKeyTitle]);
         for (const QString &titleChunk : titleChunks) {
-            queryString += QString(QStringLiteral(" title:%1")).arg(EncoderLaTeX::instance().convertToPlainAscii(titleChunk));
+            queryString += QString(QStringLiteral(" title:%1")).arg(Encoder::instance().convertToPlainAscii(titleChunk));
         }
 
         const QStringList authors = OnlineSearchAbstract::splitRespectingQuotationMarks(query[queryKeyAuthor]);
         for (const QString &author : authors) {
-            queryString += QString(QStringLiteral(" name:%1")).arg(EncoderLaTeX::instance().convertToPlainAscii(author));
+            queryString += QString(QStringLiteral(" name:%1")).arg(Encoder::instance().convertToPlainAscii(author));
         }
 
         QString year = query[queryKeyYear];

commit f99c3bebbe95ffe39171949bf8050b42893a6971

Fixing regression that '&' was not written as '\&'
Regression got introduced in commit fa2f61c53e39da37edf7f2d538.

src/io/fileexporterbibtex.cpp
@@ -32,7 +32,7 @@
 #include "preamble.h"
 #include "value.h"
 #include "comment.h"
-#include "encoder.h"
+#include "encoderlatex.h"
 #include "bibtexentries.h"
 #include "bibtexfields.h"
 #include "textencoder.h"
@@ -540,8 +540,8 @@ QString FileExporterBibTeX::valueToBibTeX(const Value &value, const QString &key
 
 QString FileExporterBibTeX::applyEncoder(const QString &input, UseLaTeXEncoding useLaTeXEncoding) const {
     switch (useLaTeXEncoding) {
-    case leLaTeX: return Encoder::instance().encode(input, Encoder::TargetEncodingASCII);
-    case leUTF8: return Encoder::instance().encode(input, Encoder::TargetEncodingUTF8);
+    case leLaTeX: return EncoderLaTeX::instance().encode(input, Encoder::TargetEncodingASCII);
+    case leUTF8: return EncoderLaTeX::instance().encode(input, Encoder::TargetEncodingUTF8);
     default: return input;
     }
 }