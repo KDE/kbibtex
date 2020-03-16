c575489e0fbfd52af8ceb39e41a65bdbfb15405e
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2020-03-16 22:58:57 +0100

    Fixing and revamping ACM's Digital Library
    Fixing and revamping ACM's Digital Library (a.k.a. ACM Portal) online search
    which became necessary due to a recent redesign of the homepage. Less HTTP
    requests are necessary now as the bibliography data for multiple items can be
    fetched in one request. Data returned by ACM is in JSON format.
    
    This is a forward-port of commit 925a20cd from branch 'kbibtex/0.10'.

--- src/networking/onlinesearch/onlinesearchacmportal.h
+++ src/networking/onlinesearch/onlinesearchacmportal.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2020 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -44,8 +44,7 @@ public:
 private slots:
     void doneFetchingStartPage();
     void doneFetchingSearchPage();
-    void doneFetchingCitation();
-    void doneFetchingBibTeX();
+    void doneFetchingJSON();
 
 private:
     class OnlineSearchAcmPortalPrivate;
