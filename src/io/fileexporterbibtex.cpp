81db327bc124f30ae42cbaa718893f2b343562db
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2020-03-21 21:56:21 +0100

    Fixing clazy warning: const Qt container for C++11-style 'for-each' loop

--- src/io/fileexporterbibtex.cpp
+++ src/io/fileexporterbibtex.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2020 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -395,7 +395,7 @@ bool FileExporterBibTeX::save(QIODevice *iodevice, const File *bibtexfile, QStri
             processedEntryIds.insert(entry->id());
 
             /// Postpone entries that are crossref'ed
-            QStringList crossRefList = crossRefMap.value(entry->id(), QStringList());
+            const QStringList crossRefList = crossRefMap.value(entry->id(), QStringList());
             if (!crossRefList.isEmpty()) {
                 bool allProcessed = true;
                 for (const QString &origin : crossRefList)
