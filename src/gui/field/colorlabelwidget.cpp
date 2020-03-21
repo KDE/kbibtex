668dab51d04307f0c363f768c9542a65b6b3d219
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2020-03-21 21:57:35 +0100

    Fixing clazy warning: global non-POD static (QColor)

--- src/gui/field/colorlabelwidget.cpp
+++ src/gui/field/colorlabelwidget.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2020 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -28,7 +28,7 @@
 #include <NotificationHub>
 #include <Preferences>
 
-static const QColor NoColor = Qt::black;
+#define NoColor QColor(Qt::black)
 
 class ColorLabelComboBoxModel : public QAbstractItemModel
 {
