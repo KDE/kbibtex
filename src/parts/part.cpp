KDE/kbibtex

commit 40a2aee94fdc14b66b6806d552b599de3b4e64c1

Migrating away from QSignalMapper
Refactoring usage of QSignalMapper by using lambda functions
in QObject::connect calls.

Minor code refactoring done at the same time ...

src/parts/part.cpp
@@ -25,7 +25,6 @@
 #include <QApplication>
 #include <QLayout>
 #include <QKeyEvent>
-#include <QSignalMapper>
 #include <QMimeDatabase>
 #include <QMimeType>
 #include <QPointer>
@@ -88,11 +87,6 @@
 #include "logging_parts.h"
 
 static const char RCFileName[] = "kbibtexpartui.rc";
-static const int smEntry = 1;
-static const int smComment = 2;
-static const int smPreamble = 3;
-static const int smMacro = 4;
-
 class KBibTeXPart::KBibTeXPartPrivate
 {
 private:
@@ -123,11 +117,8 @@ public:
     PartWidget *partWidget;
     FileModel *model;
     SortFilterFileModel *sortFilterProxyModel;
-    QSignalMapper *signalMapperNewElement;
     QAction *editCutAction, *editDeleteAction, *editCopyAction, *editPasteAction, *editCopyReferencesAction, *elementEditAction, *elementViewDocumentAction, *fileSaveAction, *elementFindPDFAction, *entryApplyDefaultFormatString;
     QMenu *viewDocumentMenu;
-    QSignalMapper *signalMapperViewDocument;
-    QSet<QObject *> signalMapperViewDocumentSenders;
     bool isSaveAsOperation;
     LyX *lyx;
     FindDuplicatesUI *findDuplicatesUI;
@@ -136,8 +127,7 @@ public:
     QFileSystemWatcher fileSystemWatcher;
 
     KBibTeXPartPrivate(QWidget *parentWidget, KBibTeXPart *parent)
-            : p(parent), bibTeXFile(nullptr), model(nullptr), sortFilterProxyModel(nullptr), signalMapperNewElement(new QSignalMapper(parent)), viewDocumentMenu(new QMenu(i18n("View Document"), parent->widget())), signalMapperViewDocument(new QSignalMapper(parent)), isSaveAsOperation(false), fileSystemWatcher(p) {
-        connect(signalMapperViewDocument, static_cast<void(QSignalMapper::*)(QObject *)>(&QSignalMapper::mapped), p, &KBibTeXPart::elementViewDocumentMenu);
+            : p(parent), bibTeXFile(nullptr), model(nullptr), sortFilterProxyModel(nullptr), viewDocumentMenu(new QMenu(i18n("View Document"), parent->widget())), isSaveAsOperation(false), fileSystemWatcher(p) {
         connect(&fileSystemWatcher, &QFileSystemWatcher::fileChanged, p, &KBibTeXPart::fileExternallyChange);
 
         partWidget = new PartWidget(parentWidget);
@@ -150,9 +140,7 @@ public:
     ~KBibTeXPartPrivate() {
         delete bibTeXFile;
         delete model;
-        delete signalMapperNewElement;
         delete viewDocumentMenu;
-        delete signalMapperViewDocument;
         delete findDuplicatesUI;
     }
 
@@ -184,20 +172,23 @@ public:
         QMenu *newElementMenu = new QMenu(newElementAction->text(), p->widget());
         newElementAction->setMenu(newElementMenu);
         connect(newElementAction, &QAction::triggered, p, &KBibTeXPart::newEntryTriggered);
-        QAction *newEntry = newElementMenu->addAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New entry"));
+
+        QAction *newEntry = new QAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New entry"), newElementAction);
+        newElementMenu->addAction(newEntry);
         p->actionCollection()->setDefaultShortcut(newEntry, Qt::CTRL + Qt::SHIFT + Qt::Key_N);
-        connect(newEntry, &QAction::triggered, signalMapperNewElement, static_cast<void(QSignalMapper::*)()>(&QSignalMapper::map));
-        signalMapperNewElement->setMapping(newEntry, smEntry);
-        QAction *newComment = newElementMenu->addAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New comment"));
-        connect(newComment, &QAction::triggered, signalMapperNewElement, static_cast<void(QSignalMapper::*)()>(&QSignalMapper::map));
-        signalMapperNewElement->setMapping(newComment, smComment);
-        QAction *newMacro = newElementMenu->addAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New macro"));
-        connect(newMacro, &QAction::triggered, signalMapperNewElement, static_cast<void(QSignalMapper::*)()>(&QSignalMapper::map));
-        signalMapperNewElement->setMapping(newMacro, smMacro);
-        QAction *newPreamble = newElementMenu->addAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New preamble"));
-        connect(newPreamble, &QAction::triggered, signalMapperNewElement, static_cast<void(QSignalMapper::*)()>(&QSignalMapper::map));
-        signalMapperNewElement->setMapping(newPreamble, smPreamble);
-        connect(signalMapperNewElement, static_cast<void(QSignalMapper::*)(int)>(&QSignalMapper::mapped), p, &KBibTeXPart::newElementTriggered);
+        connect(newEntry, &QAction::triggered, p, &KBibTeXPart::newEntryTriggered);
+
+        QAction *newComment = new QAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New comment"), newElementAction);
+        newElementMenu->addAction(newComment);
+        connect(newComment, &QAction::triggered, p, &KBibTeXPart::newCommentTriggered);
+
+        QAction *newMacro = new QAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New macro"), newElementAction);
+        newElementMenu->addAction(newMacro);
+        connect(newMacro, &QAction::triggered, p, &KBibTeXPart::newMacroTriggered);
+
+        QAction *newPreamble = new QAction(QIcon::fromTheme(QStringLiteral("address-book-new")), i18n("New preamble"), newElementAction);
+        newElementMenu->addAction(newPreamble);
+        connect(newPreamble, &QAction::triggered, p, &KBibTeXPart::newPreambleTriggered);
 
         /// Action to edit an element
         elementEditAction = new QAction(QIcon::fromTheme(QStringLiteral("document-edit")), i18n("Edit Element"), p);
@@ -642,15 +633,6 @@ public:
         File *bibliographyFile = partWidget != nullptr && partWidget->fileView() != nullptr && partWidget->fileView()->fileModel() != nullptr ? partWidget->fileView()->fileModel()->bibliographyFile() : nullptr;
         if (bibliographyFile == nullptr) return result;
 
-        /// Clean signal mapper of old mappings
-        /// as stored in QSet signalMapperViewDocumentSenders
-        /// and identified by their QAction*'s
-        QSet<QObject *>::Iterator it = signalMapperViewDocumentSenders.begin();
-        while (it != signalMapperViewDocumentSenders.end()) {
-            signalMapperViewDocument->removeMappings(*it);
-            it = signalMapperViewDocumentSenders.erase(it);
-        }
-
         /// Retrieve Entry object of currently selected line
         /// in main list view
         QSharedPointer<const Entry> entry = partWidget->fileView()->currentElement().dynamicCast<const Entry>();
@@ -671,12 +653,11 @@ public:
                     const QString label = QString(QStringLiteral("%1 [%2]")).arg(fi.fileName(), fi.absolutePath());
                     QMimeDatabase db;
                     QAction *action = new QAction(QIcon::fromTheme(db.mimeTypeForUrl(url).iconName()), label, p);
-                    action->setData(fi.absoluteFilePath());
                     action->setToolTip(fi.absoluteFilePath());
-                    /// Register action at signal handler to open URL when triggered
-                    connect(action, &QAction::triggered, signalMapperViewDocument, static_cast<void(QSignalMapper::*)()>(&QSignalMapper::map));
-                    signalMapperViewDocument->setMapping(action, action);
-                    signalMapperViewDocumentSenders.insert(action);
+                    /// Open URL when action is triggered
+                    connect(action, &QAction::triggered, p, [this, fi]() {
+                        elementViewDocumentMenu(QUrl::fromLocalFile(fi.absoluteFilePath()));
+                    });
                     viewDocumentMenu->addAction(action);
                     /// Memorize first action
                     if (firstAction == nullptr) firstAction = action;
@@ -697,12 +678,11 @@ public:
                     const QString prettyUrl = url.toDisplayString();
                     QMimeDatabase db;
                     QAction *action = new QAction(QIcon::fromTheme(db.mimeTypeForUrl(url).iconName()), prettyUrl, p);
-                    action->setData(prettyUrl);
                     action->setToolTip(prettyUrl);
-                    /// Register action at signal handler to open URL when triggered
-                    connect(action, &QAction::triggered, signalMapperViewDocument, static_cast<void(QSignalMapper::*)()>(&QSignalMapper::map));
-                    signalMapperViewDocument->setMapping(action, action);
-                    signalMapperViewDocumentSenders.insert(action);
+                    /// Open URL when action is triggered
+                    connect(action, &QAction::triggered, p, [this, url]() {
+                        elementViewDocumentMenu(url);
+                    });
                     viewDocumentMenu->addAction(action);
                     /// Memorize first action
                     if (firstAction == nullptr) firstAction = action;
@@ -733,6 +713,15 @@ public:
             break;
         }
     }
+
+    void elementViewDocumentMenu(const QUrl &url)
+    {
+        const QMimeType mimeType = FileInfo::mimeTypeForUrl(url);
+        const QString mimeTypeName = mimeType.name();
+        /// Ask KDE subsystem to open url in viewer matching mime type
+        KRun::runUrl(url, mimeTypeName, p->widget(), KRun::RunFlags());
+    }
+
 };
 
 KBibTeXPart::KBibTeXPart(QWidget *parentWidget, QObject *parent, const KAboutData &componentData)
@@ -894,22 +883,6 @@ void KBibTeXPart::elementViewDocument()
     }
 }
 
-void KBibTeXPart::elementViewDocumentMenu(QObject *obj)
-{
-    QString text = static_cast<QAction *>(obj)->data().toString(); ///< only a QAction will be passed along
-
-    /// Guess mime type for url to open
-    QUrl url(text);
-    QMimeType mimeType = FileInfo::mimeTypeForUrl(url);
-    const QString mimeTypeName = mimeType.name();
-    /// Ask KDE subsystem to open url in viewer matching mime type
-#if KIO_VERSION < 0x051f00 // < 5.31.0
-    KRun::runUrl(url, mimeTypeName, widget(), false, false);
-#else // KIO_VERSION < 0x051f00 // >= 5.31.0
-    KRun::runUrl(url, mimeTypeName, widget(), KRun::RunFlags());
-#endif // KIO_VERSION < 0x051f00
-}
-
 void KBibTeXPart::elementFindPDF()
 {
     QModelIndexList mil = d->partWidget->fileView()->selectionModel()->selectedRows();
@@ -949,23 +922,6 @@ bool KBibTeXPart::openFile()
     const bool success = d->openFile(url(), localFilePath());
     emit completed();
     return success;
-}
-
-void KBibTeXPart::newElementTriggered(int event)
-{
-    switch (event) {
-    case smComment:
-        newCommentTriggered();
-        break;
-    case smMacro:
-        newMacroTriggered();
-        break;
-    case smPreamble:
-        newPreambleTriggered();
-        break;
-    default:
-        newEntryTriggered();
-    }
 }
 
 void KBibTeXPart::newEntryTriggered()

src/gui/field/fieldinput.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2018 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -21,7 +21,6 @@
 #include <QApplication>
 #include <QMenu>
 #include <QDate>
-#include <QSignalMapper>
 #include <QSpinBox>
 #include <QPushButton>
 #include <QInputDialog>
@@ -83,12 +82,12 @@ public:
             monthSelector->setToolTip(i18n("Select a predefined month"));
             fieldLineEdit->prependWidget(monthSelector);
 
-            QSignalMapper *sm = new QSignalMapper(monthSelector);
-            connect(sm, static_cast<void(QSignalMapper::*)(int)>(&QSignalMapper::mapped), p, &FieldInput::setMonth);
             QMenu *monthMenu = new QMenu(monthSelector);
             for (int i = 1; i <= 12; ++i) {
-                QAction *monthAction = monthMenu->addAction(QDate::longMonthName(i, QDate::StandaloneFormat), sm, SLOT(map()));
-                sm->setMapping(monthAction, i);
+                QAction *monthAction = monthMenu->addAction(QLocale::system().standaloneMonthName(i));
+                connect(monthAction, &QAction::triggered, p, [this, i]() {
+                    setMonth(i);
+                });
             }
             monthSelector->setMenu(monthMenu);
         }
@@ -100,13 +99,13 @@ public:
             editionSelector->setToolTip(i18n("Select a predefined edition"));
             fieldLineEdit->prependWidget(editionSelector);
 
-            QSignalMapper *sm = new QSignalMapper(editionSelector);
-            connect(sm, static_cast<void(QSignalMapper::*)(int)>(&QSignalMapper::mapped), p, &FieldInput::setEdition);
             QMenu *editionMenu = new QMenu(editionSelector);
             static const QStringList ordinals{i18n("1st"), i18n("2nd"), i18n("3rd"), i18n("4th"), i18n("5th"), i18n("6th"), i18n("7th"), i18n("8th"), i18n("9th"), i18n("10th"), i18n("11th"), i18n("12th"), i18n("13th"), i18n("14th"), i18n("15th"), i18n("16th")};
             for (int i = 0; i < ordinals.length(); ++i) {
-                QAction *editionAction = editionMenu->addAction(ordinals[i], sm, SLOT(map()));
-                sm->setMapping(editionAction, i + 1);
+                QAction *editionAction = editionMenu->addAction(ordinals[i]);
+                connect(editionAction, &QAction::triggered, p, [this, i]() {
+                    setEdition(i + 1);
+                });
             }
             editionSelector->setMenu(editionMenu);
         }
@@ -294,6 +293,24 @@ public:
         if (starRatingWidget != nullptr)
             disconnect(starRatingWidget, &StarRatingFieldInput::modified, p, &FieldInput::modified);
     }
+
+    void setMonth(int month)
+    {
+        Value value;
+        value.append(QSharedPointer<MacroKey>(new MacroKey(KBibTeX::MonthsTriple[month - 1])));
+        reset(value);
+        /// Instead of an 'emit' ...
+        QMetaObject::invokeMethod(p, "modified", Qt::DirectConnection, QGenericReturnArgument());
+    }
+
+    void setEdition(int edition)
+    {
+        Value value;
+        value.append(QSharedPointer<MacroKey>(new MacroKey(QString::number(edition))));
+        reset(value);
+        /// Instead of an 'emit' ...
+        QMetaObject::invokeMethod(p, "modified", Qt::DirectConnection, QGenericReturnArgument());
+    }
 };
 
 FieldInput::FieldInput(KBibTeX::FieldInputType fieldInputType, KBibTeX::TypeFlag preferredTypeFlag, KBibTeX::TypeFlags typeFlags, QWidget *parent)
@@ -365,22 +382,6 @@ QWidget *FieldInput::buddy()
     else if (d->starRatingWidget != nullptr)
         return d->starRatingWidget;
     return nullptr;
-}
-
-void FieldInput::setMonth(int month)
-{
-    Value value;
-    value.append(QSharedPointer<MacroKey>(new MacroKey(KBibTeX::MonthsTriple[month - 1])));
-    reset(value);
-    emit modified();
-}
-
-void FieldInput::setEdition(int edition)
-{
-    Value value;
-    value.append(QSharedPointer<MacroKey>(new MacroKey(QString::number(edition))));
-    reset(value);
-    emit modified();
 }
 
 void FieldInput::selectCrossRef()

src/gui/field/fieldinput.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2018 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -56,8 +56,6 @@ signals:
     void modified();
 
 private slots:
-    void setMonth(int month);
-    void setEdition(int edition);
     void selectCrossRef();
 
 private:

src/gui/field/fieldlineedit.cpp
@@ -20,7 +20,6 @@
 #include <typeinfo>
 
 #include <QMenu>
-#include <QSignalMapper>
 #include <QBuffer>
 #include <QFileInfo>
 #include <QDir>
@@ -57,7 +56,6 @@ private:
     Value currentValue;
     KBibTeX::TypeFlag preferredTypeFlag;
     KBibTeX::TypeFlags typeFlags;
-    QSignalMapper *menuTypesSignalMapper;
     QPushButton *buttonOpenUrl;
 
 public:
@@ -70,9 +68,7 @@ public:
     FieldLineEditPrivate(KBibTeX::TypeFlag ptf, KBibTeX::TypeFlags tf, FieldLineEdit *p)
             : parent(p), preferredTypeFlag(ptf), typeFlags(tf), file(nullptr) {
         menuTypes = new QMenu(parent);
-        menuTypesSignalMapper = new QSignalMapper(parent);
         setupMenu();
-        connect(menuTypesSignalMapper, static_cast<void(QSignalMapper::*)(int)>(&QSignalMapper::mapped), parent, &FieldLineEdit::slotTypeChanged);
 
         buttonOpenUrl = new QPushButton(QIcon::fromTheme(QStringLiteral("document-open-remote")), QString(), parent);
         buttonOpenUrl->setVisible(false);
@@ -280,28 +276,40 @@ public:
         menuTypes->clear();
 
         if (typeFlags.testFlag(KBibTeX::tfPlainText)) {
-            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfPlainText), i18n("Plain Text"), menuTypesSignalMapper, SLOT(map()));
-            menuTypesSignalMapper->setMapping(action, KBibTeX::tfPlainText);
+            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfPlainText), i18n("Plain Text"));
+            connect(action, &QAction::triggered, parent, [this]() {
+                typeChanged(KBibTeX::tfPlainText);
+            });
         }
         if (typeFlags.testFlag(KBibTeX::tfReference)) {
-            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfReference), i18n("Reference"), menuTypesSignalMapper, SLOT(map()));
-            menuTypesSignalMapper->setMapping(action, KBibTeX::tfReference);
+            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfReference), i18n("Reference"));
+            connect(action, &QAction::triggered, parent, [this]() {
+                typeChanged(KBibTeX::tfReference);
+            });
         }
         if (typeFlags.testFlag(KBibTeX::tfPerson)) {
-            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfPerson), i18n("Person"), menuTypesSignalMapper, SLOT(map()));
-            menuTypesSignalMapper->setMapping(action, KBibTeX::tfPerson);
+            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfPerson), i18n("Person"));
+            connect(action, &QAction::triggered, parent, [this]() {
+                typeChanged(KBibTeX::tfPerson);
+            });
         }
         if (typeFlags.testFlag(KBibTeX::tfKeyword)) {
-            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfKeyword), i18n("Keyword"), menuTypesSignalMapper, SLOT(map()));
-            menuTypesSignalMapper->setMapping(action, KBibTeX::tfKeyword);
+            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfKeyword), i18n("Keyword"));
+            connect(action, &QAction::triggered, parent, [this]() {
+                typeChanged(KBibTeX::tfKeyword);
+            });
         }
         if (typeFlags.testFlag(KBibTeX::tfSource)) {
-            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfSource), i18n("Source Code"), menuTypesSignalMapper, SLOT(map()));
-            menuTypesSignalMapper->setMapping(action, KBibTeX::tfSource);
+            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfSource), i18n("Source Code"));
+            connect(action, &QAction::triggered, parent, [this]() {
+                typeChanged(KBibTeX::tfSource);
+            });
         }
         if (typeFlags.testFlag(KBibTeX::tfVerbatim)) {
-            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfVerbatim), i18n("Verbatim Text"), menuTypesSignalMapper, SLOT(map()));
-            menuTypesSignalMapper->setMapping(action, KBibTeX::tfVerbatim);
+            QAction *action = menuTypes->addAction(iconForTypeFlag(KBibTeX::tfVerbatim), i18n("Verbatim Text"));
+            connect(action, &QAction::triggered, parent, [this]() {
+                typeChanged(KBibTeX::tfVerbatim);
+            });
         }
     }
 
@@ -439,6 +447,18 @@ public:
     void textChanged(const QString &text) {
         updateURL(text);
     }
+
+    void typeChanged(const KBibTeX::TypeFlag newTypeFlag)
+    {
+        Value value;
+        apply(value);
+
+        if (convertValueType(value, newTypeFlag)) {
+            typeFlag = newTypeFlag;
+            reset(value);
+        } else
+            KMessageBox::error(parent, i18n("The current text cannot be used as value of type '%1'.\n\nSwitching back to type '%2'.", BibTeXFields::typeFlagToString(newTypeFlag), BibTeXFields::typeFlagToString(typeFlag)));
+    }
 };
 
 FieldLineEdit::FieldLineEdit(KBibTeX::TypeFlag preferredTypeFlag, KBibTeX::TypeFlags typeFlags, bool isMultiLine, QWidget *parent)
@@ -474,20 +494,6 @@ bool FieldLineEdit::validate(QWidget **widgetWithIssue, QString &message) const
 void FieldLineEdit::setReadOnly(bool isReadOnly)
 {
     MenuLineEdit::setReadOnly(isReadOnly);
-}
-
-void FieldLineEdit::slotTypeChanged(int newTypeFlagInt)
-{
-    const KBibTeX::TypeFlag newTypeFlag = static_cast<KBibTeX::TypeFlag>(newTypeFlagInt);
-
-    Value value;
-    d->apply(value);
-
-    if (d->convertValueType(value, newTypeFlag)) {
-        d->typeFlag = newTypeFlag;
-        d->reset(value);
-    } else
-        KMessageBox::error(this, i18n("The current text cannot be used as value of type '%1'.\n\nSwitching back to type '%2'.", BibTeXFields::typeFlagToString(newTypeFlag), BibTeXFields::typeFlagToString(d->typeFlag)));
 }
 
 void FieldLineEdit::setFile(const File *file)

src/gui/field/fieldlineedit.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2018 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
+ *   Copyright (C) 2004-2019 by Thomas Fischer <fischer@unix-ag.uni-kl.de> *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -27,7 +27,6 @@
 #include <widgets/MenuLineEdit>
 
 class QMenu;
-class QSignalMapper;
 
 class Element;
 
@@ -69,7 +68,6 @@ private:
     FieldLineEdit::FieldLineEditPrivate *d;
 
 private slots:
-    void slotTypeChanged(int);
     void slotTextChanged(const QString &);
     void slotOpenUrl();
 };

commit de778fc5107b24b6516ebe8d467fe7728dd46f67

'View Document' menu item failed to open URL
Reverting a regression introduced in commit 40a2aee94fdc14b66b.

Backport of commit d47e2561 from branch 'master'.

src/parts/part.cpp
@@ -654,6 +654,7 @@ public:
                     const QFileInfo fi(url.toLocalFile());
                     const QString label = QString(QStringLiteral("%1 [%2]")).arg(fi.fileName(), fi.absolutePath());
                     QAction *action = new QAction(QIcon::fromTheme(FileInfo::mimeTypeForUrl(url).iconName()), label, p);
+                    action->setData(QUrl::fromLocalFile(fi.absoluteFilePath()));
                     action->setToolTip(fi.absoluteFilePath());
                     /// Open URL when action is triggered
                     connect(action, &QAction::triggered, p, [this, fi]() {
@@ -678,6 +679,7 @@ public:
                     /// Build a nice menu item (label, icon, ...)
                     const QString prettyUrl = url.toDisplayString();
                     QAction *action = new QAction(QIcon::fromTheme(FileInfo::mimeTypeForUrl(url).iconName()), prettyUrl, p);
+                    action->setData(url);
                     action->setToolTip(prettyUrl);
                     /// Open URL when action is triggered
                     connect(action, &QAction::triggered, p, [this, url]() {
@@ -854,9 +856,8 @@ void KBibTeXPart::elementViewDocument()
     /// Go through all actions (i.e. document URLs) for this element
     for (const QAction *action : actionList) {
         /// Make URL from action's data ...
-        const QString actionData = action->data().toString();
-        if (actionData.isEmpty()) continue; ///< No URL from empty string
-        const QUrl tmpUrl = QUrl::fromUserInput(actionData);
+        const QUrl tmpUrl = action->data().toUrl();
+        if (!tmpUrl.isValid()) continue;
         /// ... but skip this action if the URL is invalid
         if (!tmpUrl.isValid()) continue;
         if (tmpUrl.isLocalFile()) {