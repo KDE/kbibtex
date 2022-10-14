e3ef9519172415beaf3114fa89105433c2882977
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2022-10-14 22:27:12 +0200

    Adding test on re-arranging crossref'ed entries

--- src/test/kbibtexdatatest.cpp
+++ src/test/kbibtexdatatest.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *   SPDX-License-Identifier: GPL-2.0-or-later
  *                                                                         *
- *   SPDX-FileCopyrightText: 2004-2018 Thomas Fischer <fischer@unix-ag.uni-kl.de>
+ *   SPDX-FileCopyrightText: 2004-2022 Thomas Fischer <fischer@unix-ag.uni-kl.de>
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -19,7 +19,10 @@
 
 #include <QtTest>
 
+#include <FileExporterBibTeX>
+#include <FileImporterBibTeX>
 #include <Entry>
+#include <Macro>
 
 class KBibTeXDataTest : public QObject
 {
@@ -34,6 +37,9 @@ private slots:
      * irregularities in memory management or access.
      */
     void createAndRemoveValueFromEntries();
+
+    void rearrangingCrossRefEntries_data();
+    void rearrangingCrossRefEntries();
 
 private:
 };
@@ -51,6 +57,87 @@ void KBibTeXDataTest::createAndRemoveValueFromEntries()
             entry.insert(key, v);
         }
     }
+}
+
+void KBibTeXDataTest::rearrangingCrossRefEntries_data()
+{
+    QTest::addColumn<File *>("originalBibTeXfile");
+    QTest::addColumn<File *>("rearrangedBibTeXfile");
+
+    QSharedPointer<Entry> aaabbb{new Entry(Entry::etBook, QStringLiteral("aaabbb"))};
+    aaabbb->insert(Entry::ftTitle, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("{Companion of the Duality of Polymorphism}"))));
+    aaabbb->insert(Entry::ftAuthor, Value() << QSharedPointer<Person>(new Person(QStringLiteral("Winston"), QStringLiteral("Smith"))));
+    aaabbb->insert(Entry::ftPublisher, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("Black Books"))));
+    aaabbb->insert(Entry::ftYear, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("2005"))));
+    QSharedPointer<Entry> bbbccc{new Entry(Entry::etArticle, QStringLiteral("bbbccc"))};
+    bbbccc->insert(Entry::ftTitle, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("{Glibberish}"))));
+    bbbccc->insert(Entry::ftAuthor, Value() << QSharedPointer<Person>(new Person(QStringLiteral("Bernard"), QStringLiteral("Marx"))));
+    bbbccc->insert(Entry::ftYear, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("2022"))));
+    bbbccc->insert(Entry::ftJournal, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("New Swabia Journal of Incomplete Algorithms"))));
+    QSharedPointer<Entry> cccddd{new Entry(Entry::etArticle, QStringLiteral("cccddd"))};
+    cccddd->insert(Entry::ftTitle, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("{Nonsense}"))));
+    cccddd->insert(Entry::ftAuthor, Value() << QSharedPointer<Person>(new Person(QStringLiteral("Lenina"), QStringLiteral("Crowne"))));
+    cccddd->insert(Entry::ftYear, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("2019"))));
+    cccddd->insert(Entry::ftJournal, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("International Journal for the Advancement of Reversal"))));
+    QSharedPointer<Entry> dddeee{new Entry(Entry::etArticle, QStringLiteral("dddeee"))};
+    dddeee->insert(Entry::ftTitle, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("{Bulls**t}"))));
+    dddeee->insert(Entry::ftAuthor, Value() << QSharedPointer<Person>(new Person(QStringLiteral("Thomas"), QStringLiteral("Grahambell"))));
+    dddeee->insert(Entry::ftYear, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("2021"))));
+    dddeee->insert(Entry::ftJournal, Value() << QSharedPointer<PlainText>(new PlainText(QString::fromUtf8("Kot und K\xc3\xb6ter"))));
+    QSharedPointer<Entry> eeefff{new Entry(Entry::etArticle, QStringLiteral("eeefff"))};
+    eeefff->insert(Entry::ftTitle, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("{Bla Bla}"))));
+    eeefff->insert(Entry::ftAuthor, Value() << QSharedPointer<Person>(new Person(QStringLiteral("Helmholtz"), QStringLiteral("Watson"))));
+    eeefff->insert(Entry::ftYear, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("1998"))));
+    eeefff->insert(Entry::ftJournal, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("New Journal of Old Things"))));
+    QSharedPointer<Entry> fffggg{new Entry(Entry::etInBook, QStringLiteral("fffggg"))};
+    fffggg->insert(Entry::ftTitle, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("{Subsurface String Theory}"))));
+    fffggg->insert(Entry::ftCrossRef, Value() << QSharedPointer<VerbatimText>(new VerbatimText(QStringLiteral("aaabbb"))));
+    fffggg->insert(Entry::ftPages, Value() << QSharedPointer<PlainText>(new PlainText(QString::fromUtf8("42\xe2\x80\x93""56"))));
+
+    File *originalBibTeXfile = new File();
+    originalBibTeXfile->append(aaabbb);
+    originalBibTeXfile->append(eeefff);
+    originalBibTeXfile->append(dddeee);
+    originalBibTeXfile->append(fffggg);
+    originalBibTeXfile->append(bbbccc);
+    originalBibTeXfile->append(cccddd);
+    File *rearrangedBibTeXfile = new File();
+    rearrangedBibTeXfile->append(eeefff);
+    rearrangedBibTeXfile->append(dddeee);
+    rearrangedBibTeXfile->append(fffggg);
+    rearrangedBibTeXfile->append(bbbccc);
+    rearrangedBibTeXfile->append(cccddd);
+    rearrangedBibTeXfile->append(aaabbb);
+    QTest::newRow("Crossref'ed to end") << originalBibTeXfile << rearrangedBibTeXfile;
+
+    originalBibTeXfile = new File();
+    originalBibTeXfile->append(eeefff);
+    originalBibTeXfile->append(dddeee);
+    originalBibTeXfile->append(fffggg);
+    originalBibTeXfile->append(aaabbb);
+    originalBibTeXfile->append(bbbccc);
+    originalBibTeXfile->append(cccddd);
+    rearrangedBibTeXfile = new File();
+    rearrangedBibTeXfile->append(eeefff);
+    rearrangedBibTeXfile->append(dddeee);
+    rearrangedBibTeXfile->append(fffggg);
+    rearrangedBibTeXfile->append(aaabbb);
+    rearrangedBibTeXfile->append(bbbccc);
+    rearrangedBibTeXfile->append(cccddd);
+    QTest::newRow("Crossref'ed after reference") << originalBibTeXfile << rearrangedBibTeXfile;
+}
+
+void KBibTeXDataTest::rearrangingCrossRefEntries()
+{
+    QFETCH(File *, originalBibTeXfile);
+    QFETCH(File *, rearrangedBibTeXfile);
+
+    FileExporterBibTeX exporter(this);
+    FileImporterBibTeX importer(this);
+
+    File *savedBibTeXfile = importer.fromString(exporter.toString(originalBibTeXfile));
+
+    QCOMPARE(*rearrangedBibTeXfile, *savedBibTeXfile);
 }
 
 void KBibTeXDataTest::initTestCase()
