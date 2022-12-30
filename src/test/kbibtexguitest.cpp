4aea6ed35b1629b3dba65a441db7567eece5eab0
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2022-12-31 00:00:04 +0100

    Fixing crash when opening .bib file
    In certain situations, also depending on Linux distributions (varying Qt
    and KDE Frameworks versions), opening a bibliography file can cause
    crashes (segmentation faults) due to an invalid model or its underlying
    data.
    
    This commit changes the order of two instructions. Now first an internal
    variable is properly set. Only then an update on the model is applied.
    
    The largest part of this commit is a new test that reproducibly crashes
    without the changed order of two instructions, but passes after the fix
    has been applied.
    
    CCBUG: 433084
    CCBUG: 453455

---
+++ src/test/kbibtexguitest.cpp
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *   Copyright (C) 2022 by Thomas Fischer <fischer@unix-ag.uni-kl.de>      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include <QtTest>
+
+#include <field/FieldLineEdit>
+#include <File>
+#include <Entry>
+#include <models/FileModel>
+#include <file/SortFilterFileModel>
+
+class KBibTeXGUITest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase();
+
+    void sortedFilterFileModelSetSourceModel();
+
+private:
+};
+
+void KBibTeXGUITest::initTestCase()
+{
+    // nothing
+}
+
+void KBibTeXGUITest::sortedFilterFileModelSetSourceModel()
+{
+    File *bibTeXfile = new File();
+    // Kirsop, Barbara, and Leslie Chan. (2005) Transforming access to research literature for developing countries. Serials Reviews, 31(4): 246–255.
+    QSharedPointer<Entry> entry(new Entry(Entry::etArticle, QStringLiteral("kirsop2005accessrelitdevcountries")));
+    bibTeXfile->append(entry);
+    entry->insert(Entry::ftTitle, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("Transforming access to research literature for developing countries"))));
+    entry->insert(Entry::ftAuthor, Value() << QSharedPointer<Person>(new Person(QStringLiteral("Barbara"), QStringLiteral("Kirsop"))) << QSharedPointer<Person>(new Person(QStringLiteral("Leslie"), QStringLiteral("Chan"))));
+    entry->insert(Entry::ftYear, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("2005"))));
+    entry->insert(Entry::ftJournal, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("Serials Reviews"))));
+    entry->insert(Entry::ftVolume, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("31"))));
+    entry->insert(Entry::ftNumber, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("4"))));
+    entry->insert(Entry::ftPages, Value() << QSharedPointer<PlainText>(new PlainText(QStringLiteral("246--255"))));
+
+    QPointer<FileModel> model = new FileModel();
+    model->setBibliographyFile(bibTeXfile);
+    QPointer<SortFilterFileModel> sortFilterProxyModel = new SortFilterFileModel();
+    sortFilterProxyModel->setSourceModel(model.data());
+    QCOMPARE(sortFilterProxyModel->rowCount(), 1);
+}
+
+QTEST_MAIN(KBibTeXGUITest)
+
+#include "kbibtexguitest.moc"
