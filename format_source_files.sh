3140a9dbe8bc8d3cbbded640861ce97598b238bf
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2025-01-13 23:16:39 +0100

    Various improvements in C++ code formatting script
    Changes inspired by shellcheck.

--- format_source_files.sh
+++ format_source_files.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 export LC_ALL=en_US.utf8
 export LANG=C
 MY_NAME="$(basename "$0")"
 
-SET_VERBOSE=0
+CURYEAR="$(date '+%Y')"
 ALL_FILES=0
 HEAD_COMMIT=0
 
 function parsearguments {
-	local ARGS=$(getopt -o "h" -l "verbose,all,head,help" -n "${MY_NAME}" -- "$@")
-	local PRINT_HELP=0
+	local ARGS PRINT_HELP
+	ARGS=$(getopt -o "h" -l "verbose,all,head,help" -n "${MY_NAME}" -- "$@")
+	GETOPT_EXIT=$?
+	PRINT_HELP=0
 
 	#Bad arguments
-	if [ $? -ne 0 ] ; then
-		exit 1
-	fi
+	(( GETOPT_EXIT == 0 )) || exit 1
 
 	# A little magic
 	eval set -- "$ARGS"
@@ -24,10 +24,6 @@ function parsearguments {
 		case "$1" in
 		-h|--help)
 			PRINT_HELP=1
-			shift
-		;;
-		--verbose )
-			SET_VERBOSE=1
 			shift
 		;;
 		--all )
@@ -79,7 +75,7 @@ fi | \
 # Sort files alphabetically, omit duplicates
 sort -u | \
 grep --color=NEVER -vE '(src/config/preferences.|src/networking/onlinesearch/onlinesearch[^.]+-parser.in.)(cpp|h)$' | \
-while read filename ; do
+while read -r filename ; do
 	if [[ ! -s "${filename}" ]] ; then
 		echo "${MY_NAME}: File not found: \"${filename}\"" >&2
 		continue
@@ -88,48 +84,47 @@ while read filename ; do
 	TEMPFILE=$(mktemp -t 'kbibtex_formatted_source_XXXXXXXXXXX.cpp')
 
 	echo "Processing \"${filename}\""
-	astyle -n --align-reference=name --align-pointer=name --indent=spaces=4 --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs <"${filename}" >${TEMPFILE}
+	astyle -n --align-reference=name --align-pointer=name --indent=spaces=4 --indent-labels --pad-oper --unpad-paren --pad-header --keep-one-line-statements --convert-tabs <"${filename}" >"${TEMPFILE}" || exit 1
 
 	# Astyle has problem with 'foreach' statements
-	sed -i -e 's/\bforeach(/foreach (/g' ${TEMPFILE}
+	sed -i -e 's/\bforeach(/foreach (/g' "${TEMPFILE}" || exit 1
 
 	# normalize SIGNAL statements as recommended at
 	# http://marcmutz.wordpress.com/effective-qt/prefer-to-use-normalised-signalslot-signatures/
 	# astyle would insert spaces etc
-	grep -Po '(SIGNAL|SLOT)\([^(]+\([^)]*\)\)' "${TEMPFILE}" | while read original ; do
-		normalized="$(sed -e 's/const //g;s/&//g;s/ //g;s/\//\\\//g' <<<${original})"
-		originalregexpized="$(sed -e 's/\*/\\*/g;s/\//\\\//g' <<<${original})"
-		sed -i -e 's/'"${originalregexpized}"'/'"${normalized}"'/g' ${TEMPFILE}
-	done
+	while read -r original ; do
+		normalized="$(sed -e 's/const //g;s/&//g;s/ //g;s/\//\\\//g' <<<"${original}")"
+		originalregexpized="$(sed -e 's/\*/\\*/g;s/\//\\\//g' <<<"${original}")"
+		sed -i -e 's/'"${originalregexpized}"'/'"${normalized}"'/g' "${TEMPFILE}"
+	done < <(grep -Po '(SIGNAL|SLOT)\([^(]+\([^)]*\)\)' "${TEMPFILE}") || exit 1
 
 	# correct some astyle mis-formattings
 	if [[ "${filename}" != "${filename/%.h/}" ]] ; then
-		sed -i -e 's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' ${TEMPFILE}
+		sed -i -e 's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g' "${TEMPFILE}"
 	elif [[ "${filename}" != "${filename/%.cpp/}" ]] ; then
-		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' ${TEMPFILE}
-	fi
+		sed -i -e  's/^\([ ]*\)\(:[ ][a-zA-Z]\)/\1    \2/g;s/\(foreach([^)]*\) \([&*]\) /\1 \2/g' "${TEMPFILE}"
+	fi || exit 1
 
 	# In const_cast operations, ensure space before ampersand
-	sed -i -r 's!( : const_cast<[^(;]+[^ ])&>[(]!\1 \&>(!g' ${TEMPFILE}
+	sed -i -r 's!( : const_cast<[^(;]+[^ ])&>[(]!\1 \&>(!g' "${TEMPFILE}" || exit 1
 
 	# Remove superfluous empty lines at file's end
 	#  http://unix.stackexchange.com/a/81687
-        #  http://sed.sourceforge.net/sed1line.txt
-	sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' ${TEMPFILE}
+	#  http://sed.sourceforge.net/sed1line.txt
+	sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' "${TEMPFILE}" || exit 1
 
 	# Check if copyright year is up to date
-	head -n 5 ${TEMPFILE} | grep -Eo --color=NEVER '20[0-9][0-9] Thomas' | head -n 1 | while read year rest ; do
-		[ "${year}" = $(date '+%Y') ] || echo "File '${filename}' has an outdated copyright year: ${year}"
-	done
+	while read -r year rest ; do
+		[ "${year}" = "${CURYEAR}" ] || echo "File '${filename}' has an outdated copyright year: ${year}"
+	done < <(head -n 5 "${TEMPFILE}" | grep -Eo --color=NEVER '20[0-9][0-9] Thomas' | head -n 1)
 
 	# only change/touch original file if it has been changed
-	diff -q ${TEMPFILE} "${filename}" >/dev/null || { echo "Updating \"${filename}\"" ; cp -p ${TEMPFILE} "${filename}" || echo "Cannot copy \"${TEMPFILE}\" \"${filename}\"" ; }
+	diff -q "${TEMPFILE}" "${filename}" >/dev/null || { echo "Updating \"${filename}\"" ; cp -p "${TEMPFILE}" "${filename}" || echo "Cannot copy \"${TEMPFILE}\" \"${filename}\"" ; }
 
-	rm -f ${TEMPFILE}
+	rm -f "${TEMPFILE}" || exit 1
 done
 
-year=$(date '+%Y')
-test -s README && { grep -q "2004-${year} Thomas Fischer" README || echo "README should have current year in copyright: ${year}" ; }
+test -s README && { grep -q "2004-${CURYEAR} Thomas Fischer" README || echo "README should have current year in copyright: ${CURYEAR}" ; }
 for copyrightfile in src/program/program.cpp src/test/main.cpp src/parts/partfactory.cpp ; do
-    test -s "${copyrightfile}" && { grep -q "SPDX-FileCopyrightText: 2004-${year} Thomas Fischer" "${copyrightfile}" || echo "'${copyrightfile}' should have current year in copyright: ${year}" ; }
+	test -s "${copyrightfile}" && { grep -q "SPDX-FileCopyrightText: 2004-${CURYEAR} Thomas Fischer" "${copyrightfile}" || echo "'${copyrightfile}' should have current year in copyright: ${CURYEAR}" ; }
 done
