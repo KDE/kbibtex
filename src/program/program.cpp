c824023cacc5e71e0410b62d7ac8480697b88e9f
Thomas Fischer <fischer@unix-ag.uni-kl.de>
2025-08-26 19:51:52 +0200

    Fixing issues and warnings with localization
    - Translation domain set via compilation flag, not in C++ source code.
    - Fixing wrong numbering in BibLaTeX configuration which caused empty strings
      that failed to get translated.

--- src/program/program.cpp
+++ src/program/program.cpp
@@ -50,8 +50,6 @@ int main(int argc, char *argv[])
 
     KCrash::initialize();
 
-    KLocalizedString::setApplicationDomain("kbibtex");
-
     KAboutData aboutData(QStringLiteral("kbibtex"), i18n("KBibTeX"), strlen(KBIBTEX_GIT_INFO_STRING) > 0 ? QLatin1String(KBIBTEX_GIT_INFO_STRING ", near " KBIBTEX_VERSION_STRING) : QLatin1String(KBIBTEX_VERSION_STRING), i18n("A BibTeX editor by KDE"), KAboutLicense::GPL_V2, i18n("Copyright 2004-2024 Thomas Fischer"), QString(), QStringLiteral("https://userbase.kde.org/KBibTeX"));
 
     aboutData.setOrganizationDomain(QByteArray("kde.org"));
